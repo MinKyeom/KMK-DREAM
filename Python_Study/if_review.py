34660e3a93df7f322e6a8dacc0d12d2b05f09bff
minkyeom <minkyeom@naver.com>
2022-12-16 18:05:31 +0900

    Python_review

--- Python_Study/if_review.py
+++ Python_Study/if_review.py
@@ -79,6 +79,23 @@ else:
     print("겨울 입니다.") #해당 조건이 없다면 그냥 코드는 진행된다 오류는 안나온다 참고!
 
 
+# False로 변환되는 값
+
+print("# if 조건문에 0 넣기")
+
+if 0:
+    print("0은 True로 변환됩니다")
+
+else:
+    print("0은 False로 변환됩니다.")
+
+print()
+
+print("#if 조건문에 빈 문자열 넣기")
+if"":
+    print("빈 문자열은 True로 변환됩니다.")
+else:
+    print("빈 문자열은 False로 변환됩니다.")
 
 
 
