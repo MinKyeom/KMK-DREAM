e0cc53ce2001986194e73f58f74866ce3c56247f
minkyeom <minkyeom@naver.com>
2023-02-10 20:14:37 +0900

    python_review

--- Python_Study/Function_review2.py
+++ Python_Study/Function_review2.py
@@ -8,3 +8,16 @@ def factorial(n):
 
 print("factorial(1):",factorial(1))
 print("factorial(2):",factorial(2))
+
+#재귀 함수를 사용해 팩토리얼 구하기
+print()
+print()
+def factorial_1(n):
+
+    if n==0:
+        return 1
+    else:
+        return n*factorial_1(n-1)
+
+print(factorial_1(1))
+print(factorial_1(2))
