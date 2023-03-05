34f183e91a75ad164375cd3e24ea97b598664118
minkyeom <minkyeom@naver.com>
2023-03-05 15:58:49 +0900

    Algorism_review

--- BaekJoon/BeakJoon_1987/BeakJoon_1987.py
+++ BaekJoon/BeakJoon_1987/BeakJoon_1987.py
@@ -1,27 +1,50 @@
-R,C = map(int,input().split())
+# R,C = map(int,input().split())
+#
+# map=[] #초기 보드의 모양
+#
+# for i in range(R):
+#     map.append(list(map(int,input().split())))
+#
+# #내가 떠오른 idea: 이동하고 재귀함수로 이동하는거 반복해서 움직인 숫자의 max 또는 len
+#
+# dx=[-1,0,1,0]
+# dy=[0,-1,0,1]
+#
+# horse=[]
+# horse_arrive_list=[]
+#
+# horse[0]=map[0][0]
+#
+# def horse_move(x,y):
+#     horse[x][y]=map[x][y]
+#     for a,b in zip(dx,dy):
+#         if 0<=x+a<=R-1 and 0<=y+b<=C-1:
+#             c=map[x+a][y+b]
+#             if c not in horse:
+#                 horse_arrive_list.append(c)
 
-map=[] #초기 보드의 모양
-
-for i in range(R):
-    map.append(list(map(int,input().split())))
-
-#내가 떠오른 idea: 이동하고 재귀함수로 이동하는거 반복해서 움직인 숫자의 max 또는 len
-
+#연습 및 사고 과정 따라가기
+import sys
+R,C =map(int,sys.stdin.readline().split())
+board = [list(sys.stdin.readline().strip()) for _ in range(R)]
 dx=[-1,0,1,0]
 dy=[0,-1,0,1]
+answer =1
 
-horse=[]
-horse_arrive_list=[]
-
-horse[0]=map[0][0]
-
-def horse_move(x,y):
-    horse[x][y]=map[x][y]
-    for a,b in zip(dx,dy):
-        if 0<=x+a<=R-1 and 0<=y+b<=C-1:
-            c=map[x+a][y+b]
-            if c not in horse:
-                horse_arrive_list.append(c)
-
-
+def BFS(x,y):
+    global answer
+    q =set([(x,y,board[x][y])])  #set은 집합과 비슷한 개념!
+    while q:
+        x,y,ans =q.pop()
+        for i in range(4):
+            nx = x+dx[i]
+            ny = y+dy[i]
+            print(nx,ny)
+            if((0<=nx<R) and (0<=ny<C)) and (board[nx][ny] not in ans):
+                q.add((nx,ny,ans + board[nx][ny])) #set의 add는 원소 추가!
+                answer= max(answer,len(ans)+1)
+                print(q)
+                print(answer)
+BFS(0,0)
+print(answer)
 
--- BaekJoon/test.py
+++ BaekJoon/test.py
@@ -44,22 +44,73 @@
 #     count += x
 #
 # print(count)
-list=[]
+# list=[]
 
-def fuction(x,y):
-   new_x=x+1
-   new_y=y+1
-   x=new_x
-   y=new_y
-   print(x,y)
-
-a=[(1,2),(3,2)]
-
-for c,d in a:
-    fuction(c,d)
-
-print(a)
+# def fuction(x,y):
+#    new_x=x+1
+#    new_y=y+1
+#    x=new_x
+#    y=new_y
+#    print(x,y)
+#
+# a=[(1,2),(3,2)]
+#
+# for c,d in a:
+#     fuction(c,d)
+#
+# print(a)
     #print(c,d) # 1 2 # 3 2 출력
 
 # for c,d in a:
 #     fuction(c,d)
+
+
+# list_map=[180,200,130,50,50,150,50]
+#
+# count=0
+#
+# for x in list_map:
+#    count += x
+#
+# print(count)
+
+
+# def test(x,y):
+#     x +=1
+#     y +=1
+#     print(x,y)
+#     return x,y
+
+x,y=1,2
+
+def test(x,y):
+    global new_x,new_y
+    new_x =x+1
+    new_y =y+1
+    print(x,y)
+    return new_x,new_y
+
+
+def test_2(c,d):
+    global c,d
+    c =c+1
+    d =d+1
+    return c,d
+
+print(x)
+print()
+print(y)
+print("------")
+print()
+print("---함수 실행 전---")
+print("실험")
+test(x,y)
+print("---함수 실행 후---")
+print(new_x)
+print()
+print(new_y)
+print("--------")
+v=1
+w=1
+test_2(3,4)
+
