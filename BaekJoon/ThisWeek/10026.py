554b07dda08281bda513b06b8fb2781fad4b1b81
minkyeom <minkyeom@naver.com>
2023-04-02 18:02:24 +0900

    algorism_review

--- BaekJoon/ThisWeek/10026.py
+++ BaekJoon/ThisWeek/10026.py
@@ -3,5 +3,22 @@ n=int(input())
 pic=[]
 for a in range(n):
     pic.append(list(map(str,sys.stdin.readline())))
+dx=[-1,0,1,0]
+dy=[0,-1,0,1]
+
+def find (x,y): #주변 탐지
+    count=0
+    for e,f in dx,dy:
+        if pic[x+e][y+f]==pic[x][y]:
+            count+=1
+    return count
+
+for b in range(n):
+    for c in range(n):
+        z=find(b,c)
+        if z>0:
+            continue
+
+
 
 
