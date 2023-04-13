6707352941be33605e2a4c8f0592d5b81b5185ad
minkyeom <minkyeom@naver.com>
2023-04-13 19:10:52 +0900

    algorism_review

--- BaekJoon/3_BeakJoon_gold/BaekJoon_1700/BeakJoon_1700_2.py
+++ BaekJoon/3_BeakJoon_gold/BaekJoon_1700/BeakJoon_1700_2.py
@@ -22,53 +22,58 @@ for a in electric:
 print(use)
 """
 
-import sys
 
-input = sys.stdin.readline
+def BeakJoon_1700(N,K,multitap):
+    import sys
 
-N, K = map(int, input().split())
+    input = sys.stdin.readline
 
-multitap = list(map(int, input().split()))
+    # N, K = map(int, input().split())
+    #
+    # multitap = list(map(int, input().split()))
 
-plugs = []
-count = 0
+    plugs = []
+    count = 0
 
-for i in range(K):
+    for i in range(K):
     # 있으면 건너 뛴다.
-    if multitap[i] in plugs:
-        continue
+        if multitap[i] in plugs:
+            continue
 
     # 플러그가 1개라도 비어 있으면 집어넣는다.
-    if len(plugs) < N:
-        plugs.append(multitap[i])
-        continue
+        if len(plugs) < N:
+            plugs.append(multitap[i])
+            continue
 
-    multitap_idxs = []  # 다음 멀티탭의 값을 저장.
-    hasplug = True
+        multitap_idxs = []  # 다음 멀티탭의 값을 저장.
+        hasplug = True
 
-    for j in range(N):
+        for j in range(N):
         # 멀티탭 안에 플러그 값이 있다면
-        if plugs[j] in multitap[i:]:
+            if plugs[j] in multitap[i:]:
             # 멀티탭 인덱스 위치 값 가져오기.
-            multitap_idx = multitap[i:].index(plugs[j])
-        else:
-            multitap_idx = 101
-            hasplug = False
+                multitap_idx = multitap[i:].index(plugs[j])
+            else:
+                multitap_idx = 101
+                hasplug = False
 
         # 인덱스에 값을 넣어준다.
-        multitap_idxs.append(multitap_idx)
+            multitap_idxs.append(multitap_idx)
 
         # 없다면 종료
-        if not hasplug:
-            break
+            if not hasplug:
+                break
 
     # 플러그를 뽑는다.
-    plug_out = multitap_idxs.index(max(multitap_idxs))
-    del plugs[plug_out]  # 플러그에서 제거
-    plugs.append(multitap[i])  # 플러그에 멀티탭 값 삽입
-    count += 1  # 뽑았으므로 1 증가
+        plug_out = multitap_idxs.index(max(multitap_idxs))
+        del plugs[plug_out]  # 플러그에서 제거
+        plugs.append(multitap[i])  # 플러그에 멀티탭 값 삽입
+        count += 1  # 뽑았으므로 1 증가
 
-print(count)
+    return count
+
+a=BeakJoon_1700(2,7,[2,3,2,3,1,2,7])
+print(a)
 
 
 
--- BaekJoon/test.py
+++ BaekJoon/test.py
@@ -1,5 +1,14 @@
-a=list(map(int,input().split()))
+# a=[1,2,3,4,5]
+# b=101
+# a.append(b)
+# print(a)
+# print(a.index(max(a)))
+a=[1,2,3,4,5]
+b=[1,2,3]
+for x in a:
+    if x in b:
+        continue
 
-print(a)
+    print(1)
 
 
