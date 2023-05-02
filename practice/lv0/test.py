51dc14ebe222b00631e079f665f4981a74978821
minkyeom <minkyeom@naver.com>
2023-05-02 17:45:45 +0900

    algorism_review

--- practice/lv0/test.py
+++ practice/lv0/test.py
@@ -1,8 +1,34 @@
-from itertools import combinations
-a=[[1, 4], [9, 2], [3, 8], [11, 6]]
+# def solution(num_list, n):
+#     num=[]
+#     num_new=[]
+#     count=0
+#     while True:
+#         for a in range(n):
+#             if len(num_list)==0:
+#                 break
+#             x=num_list.pop(0)
+#             num.append(x)
+#         print(num)
+#         num_new.append(num)
+#         print(num_new)
+#         num.clear()
+#         if len(num_list)==0:
+#             return num_new
+#
 
-b=list(combinations(a,2))
+# a=[[1,2],[5,6],[7,9]]
+# print(a[0:2])
 
+
+a=[[1,2]]
+b=[]
+b.append(a)
+print(b,"시작")
+a.clear()
+a=[[1,2]]
+b.append(a)
 print(b)
-
-#[([1, 4], [9, 2]), ([1, 4], [3, 8]), ([1, 4], [11, 6]), ([9, 2], [3, 8]), ([9, 2], [11, 6]), ([3, 8], [11, 6])]
+a.clear()
+a.append(1)
+b.append(a)
+print(b)
