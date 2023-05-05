01743cb39e8a6bd1e5f1d8c1c971e856f8ad86ba
minkyeom <minkyeom@naver.com>
2023-05-05 16:55:30 +0900

    algorism_review

---
+++ practice/lv0/가위 바위 보.py
@@ -0,0 +1,31 @@
+# 내 풀이
+
+def solution(rsp):
+    a = list(rsp)
+    result = []
+    for x in a:
+        if x == "2":
+            result.append("0")
+        elif x == "0":
+            result.append("5")
+        elif x == "5":
+            result.append("2")
+    answer = "".join(result)
+
+    return answer
+
+
+# 다른 사람 풀이
+
+def solution(rsp):
+    d = {'0':'5','2':'0','5':'2'}
+    return ''.join(d[i] for i in rsp)
+
+def solution(rsp):
+    rsp =rsp.replace('2','s')
+    rsp =rsp.replace('5','p')
+    rsp =rsp.replace('0','r')
+    rsp =rsp.replace('r','5')
+    rsp =rsp.replace('s','0')
+    rsp =rsp.replace('p','2')
+    return rsp
