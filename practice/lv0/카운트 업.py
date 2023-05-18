5480b5691eafc9043abecb9a54808a5eb9275357
minkyeom <minkyeom@naver.com>
2023-05-18 18:28:04 +0900

    algorism_review

---
+++ practice/lv0/카운트 업.py
@@ -0,0 +1,10 @@
+# 내 풀이
+def solution(start, end):
+    answer = []
+    for x in range(start, end + 1):
+        answer.append(x)
+
+    return answer
+# 다른 사람 풀이
+def solution(start, end):
+    return list(range(start, end + 1))
