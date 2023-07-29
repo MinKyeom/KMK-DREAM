fae70d87eefcb4687a7a146176a6089af8aaaf0a
minkyeom <minkyeom@naver.com>
2023-07-29 12:46:22 +0900

    algorism_review

--- Programmers/lv2/광물캐기.py
+++ Programmers/lv2/광물캐기.py
@@ -1,9 +1,9 @@
-# 내 풀이
+# 내 풀이 1
 def solution(picks, minerals):
     from collections import deque
 
     answer = 0
-    tired = [[1, 1, 1], [5, 1, 1], [25, 1, 1]]
+    tired = [[1, 1, 1], [5, 1, 1], [25, 5, 1]]
     connection = {"diamond": 0,
                   "iron": 1,
                   "stone": 2
@@ -38,4 +38,70 @@ def solution(picks, minerals):
                 return answer
 
     return answer
+
+# 내 풀이 2
+def solution(picks, minerals):
+    from collections import deque
+    minerals = minerals[:5 * sum(picks)]
+    count = 0  # 광물카운트
+    c_d = 0
+    c_i = 0
+    c_s = 0
+    check = []  # 각 종류별 곡쾡이 피로도
+
+    for x in range(len(minerals)):
+        if minerals[x] == "diamond":
+            count += 1
+            c_d += 1
+            c_i += 5
+            c_s += 25
+        elif minerals[x] == "iron":
+            count += 1
+            c_d += 1
+            c_i += 1
+            c_s += 5
+        elif minerals[x] == "stone":
+            count += 1
+            c_d += 1
+            c_i += 1
+            c_s += 1
+        if count == 5 or x == len(minerals) - 1:
+            check.append([c_d, c_i, c_s])
+            count = 0
+            c_d = 0
+            c_i = 0
+            c_s = 0
+    check.sort(key=lambda x: (-x[2], -x[1], -x[0]))
+
+    result = 0
+
+    check = deque(check)
+
+    for y in range(len(picks)):
+        count_p = 0
+        while count_p < picks[y] and check:
+            result += check[0][y]
+            check.popleft()
+            count_p += 1
+
+    return result
+
+# 다른 사람 풀이
+def solution(picks, minerals):
+    def solve(picks, minerals, fatigue):
+        if sum(picks) == 0 or len(minerals) == 0:
+            return fatigue
+        result = [float('inf')]
+        for i, fatigues in enumerate(({"diamond": 1, "iron": 1, "stone": 1},
+                                      {"diamond": 5, "iron": 1, "stone": 1},
+                                      {"diamond": 25, "iron": 5, "stone": 1},)):
+            if picks[i] > 0:
+                temp_picks = picks.copy()
+                temp_picks[i] -= 1
+                result.append(
+                    solve(temp_picks, minerals[5:], fatigue + sum(fatigues[mineral] for mineral in minerals[:5])))
+        return min(result)
+
+    return solve(picks, minerals, 0)
+
 
