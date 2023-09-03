ee956f3bf9c21d168422dcbce683a2159f8c6e96
minkyeom <minkyeom@naver.com>
2023-09-03 15:53:12 +0900

    algorism_review

--- Programmers/lv2/마법의 엘리베이터.py
+++ Programmers/lv2/마법의 엘리베이터.py
@@ -36,3 +36,31 @@ def solution(storey):
         return min(storey, 11 - storey)
     left = storey % 10
     return min(left + solution(storey // 10), 10 - left + solution(storey // 10 + 1))
+
+
+# 풀이2
+def solution(storey):
+    answer = 0
+
+    while(storey!=0):
+        n = storey%10
+        num = 0
+        if n < 5:
+            num = n
+            storey -= n
+        elif n > 5:
+            num = 10 - n
+            storey += num
+        else:
+            num = 5
+
+
+        if num == 5:
+            _storey = storey // 10
+            if _storey % 10 >= 5:
+                storey += num
+            elif _storey % 10 < 5:
+                storey -= num
+        storey = storey // 10
+        answer += num
+    return answer
