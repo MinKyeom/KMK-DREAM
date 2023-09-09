aba2dbc6a34f5964c71399b465fc878a2fda2dd6
minkyeom <minkyeom@naver.com>
2023-09-09 14:44:42 +0900

    algorism_review

--- Programmers/lv2/디펜스 게임.py
+++ Programmers/lv2/디펜스 게임.py
@@ -10,3 +10,22 @@ def solution(n, k, enemy):
     answer=0
     
     return answer
+
+# 다른 사람 풀이
+
+from heapq import heappop, heappush
+
+
+def solution(n, k, enemy):
+    answer, sumEnemy = 0, 0
+    heap = []
+
+    for e in enemy:
+        heappush(heap, -e)
+        sumEnemy += e
+        if sumEnemy > n:
+            if k == 0: break
+            sumEnemy += heappop(heap)
+            k -= 1
+        answer += 1
+    return answer
