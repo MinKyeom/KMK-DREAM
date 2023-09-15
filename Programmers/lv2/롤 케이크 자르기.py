8033910c9fd4019d462e890d54034d91fd09887c
minkyeom <minkyeom@naver.com>
2023-09-15 16:46:09 +0900

    algorism_review

--- Programmers/lv2/롤 케이크 자르기.py
+++ Programmers/lv2/롤 케이크 자르기.py
@@ -47,3 +47,23 @@ def solution(topping):
     answer = max(idx2 - idx1 + 1, 0)
 
     return answer
+
+# 다른 사람 풀이
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+    dic = Counter(topping)
+    set_dic = set()
+    answer = 0
+
+    for i in topping:
+        dic[i] -= 1
+        set_dic.add(i)
+        if dic[i] == 0:
+            dic.pop(i)
+        if len(dic) == len(set_dic):
+            answer += 1
+
+    return answer
+ㄴ
