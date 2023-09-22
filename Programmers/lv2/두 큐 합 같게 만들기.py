0f93d095fa48334d354891640500e89027a7d071
minkyeom <minkyeom@naver.com>
2023-09-22 17:02:29 +0900

    algorism_review

--- Programmers/lv2/두 큐 합 같게 만들기.py
+++ Programmers/lv2/두 큐 합 같게 만들기.py
@@ -64,3 +64,20 @@ def solution(que1, que2):
             start += 1
         ans += 1
     return ans
+
+# 다른 사람 풀이
+def solution(queue1, queue2):
+    indicator2=sum(queue1)-int(sum(queue1+queue2)/2)
+    answer=0
+    sub_list=(queue1+queue2+queue1)[::-1]
+    add_list=(queue2+queue1+queue2)[::-1]
+    while indicator2!=0:
+        try:
+            if indicator2>0:
+                indicator2-=sub_list.pop()
+            else:
+                indicator2+=add_list.pop()
+        except:
+            return -1
+        answer+=1
+    return answer
