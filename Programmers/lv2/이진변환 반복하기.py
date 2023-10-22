f4b4b3af9e780047fa492259c5ef4c36510d6c5b
minkyeom <minkyeom@naver.com>
2023-10-22 15:36:01 +0900

    algorism_review

---
+++ Programmers/lv2/이진변환 반복하기.py
@@ -0,0 +1,26 @@
+# 내 풀이
+def solution(s):
+    count = 0
+    count_0 = 0
+
+    while True:
+        count += 1
+        count_0 += s.count("0")
+        s = s.replace("0", "")
+        s = len(s)
+        s = str(bin(s))[2:]
+
+        if s == "1":
+            return [count, count_0]
+
+    return 0
+
+# 다른 사람 풀이
+def solution(s):
+    a, b = 0, 0
+    while s != '1':
+        a += 1
+        num = s.count('1')
+        b += len(s) - num
+        s = bin(num)[2:]
+    return [a, b]
