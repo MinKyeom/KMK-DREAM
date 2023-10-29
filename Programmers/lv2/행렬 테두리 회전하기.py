71614c7ff1b420cbb642964e132fbc92e23c6ce5
minkyeom <minkyeom@naver.com>
2023-10-29 17:17:46 +0900

    algorism_review

--- Programmers/lv2/행렬 테두리 회전하기.py
+++ Programmers/lv2/행렬 테두리 회전하기.py
@@ -21,6 +21,7 @@ def solution(rows, columns, queries):
         one = c.pop()
 
         # 맨 윗줄
+
         for d in range(two - 2, four - 2):
             check[one - 1][d] = map[one - 1][d + 1]
 
@@ -29,7 +30,13 @@ def solution(rows, columns, queries):
         for e in range(one - 2, three - 2):
             check[e][four - 1] = map[e + 1][four - 1]
             print(check[e][four - 1])
-        print("check")
+        print("확")
+        # 맨 아랫줄
+
+        for f in range(four - 2, two - 2, -1):
+            check[three - 1][f] = map[three - 1][f + 1]
+            print(map[three - 1][f], f, three - 1)
+        print("인")
 
     answer = []
     return answer
