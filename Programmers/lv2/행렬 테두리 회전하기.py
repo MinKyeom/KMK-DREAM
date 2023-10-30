79a9cb63af6207dfb582990b6d1ebf1c8431bb82
minkyeom <minkyeom@naver.com>
2023-10-30 21:14:03 +0900

    algorism_review

--- Programmers/lv2/행렬 테두리 회전하기.py
+++ Programmers/lv2/행렬 테두리 회전하기.py
@@ -1,8 +1,9 @@
 # 내 풀이(개선 중)
 def solution(rows, columns, queries):
+    import copy
     from collections import deque
     result = []
-    map = [[False] * columns for a in range(rows)]
+    map = [[False for col in range(columns)] for a in range(rows)]
     count = 1
 
     # 기본 맵
@@ -13,32 +14,50 @@ def solution(rows, columns, queries):
 
     # 맵 모양 변경
     for c in queries:
+        check = copy.deepcopy(map)
+        # check=map.copy()
         num_list = []
-        check = map.copy()
-        four = c.pop()
-        three = c.pop()
-        two = c.pop()
-        one = c.pop()
 
-        # 맨 윗줄
+        x_1 = c[0]
+        y_1 = c[1]
+        x_2 = c[2]
+        y_2 = c[3]
 
-        for d in range(two - 2, four - 2):
-            check[one - 1][d] = map[one - 1][d + 1]
+        # 맨 윗줄 ok
 
-        # 맨 오른쪽
+        for d in range(y_1, y_2):
+            check[x_1 - 1][d] = map[x_1 - 1][d - 1]
+            num_list.append(check[x_1 - 1][d])
 
-        for e in range(one - 2, three - 2):
-            check[e][four - 1] = map[e + 1][four - 1]
-            print(check[e][four - 1])
-        print("확")
-        # 맨 아랫줄
+        # num_list.append("first")
 
-        for f in range(four - 2, two - 2, -1):
-            check[three - 1][f] = map[three - 1][f + 1]
-            print(map[three - 1][f], f, three - 1)
-        print("인")
+        # 맨 오른쪽 ok
 
-    answer = []
-    return answer
+        for e in range(x_1, x_2):
+            check[e][y_2 - 1] = map[e - 1][y_2 - 1]
+            num_list.append(check[e][y_2 - 1])
 
+        # num_list.append("second")
+
+        # 맨 아랫줄 ok
+
+        for f in range(y_1 - 1, y_2 - 1):
+            check[x_2 - 1][f] = map[x_2 - 1][f + 1]
+            num_list.append(check[x_2 - 1][f])
+
+        # num_list.append("third")
+
+        # 맨 왼쪽
+
+        for g in range(x_1 - 1, x_2 - 1):
+            check[g][y_1 - 1] = map[g + 1][y_1 - 1]
+            num_list.append(check[g][y_1 - 1])
+
+        # num_list.append("fourth")
+
+        result.append(min(num_list))
+
+        map = check
+
+    return result
 # 다른 사람 풀이
