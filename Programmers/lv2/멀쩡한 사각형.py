2669c8eeab7b6595b0b8e84fa0f847bd0b491ce7
minkyeom <minkyeom@naver.com>
2023-11-20 15:05:22 +0900

    algorism_review

--- Programmers/lv2/멀쩡한 사각형.py
+++ Programmers/lv2/멀쩡한 사각형.py
@@ -22,3 +22,8 @@ def solution(w, h):
 # 다른 사람 풀이
 def gcd(a,b): return b if (a==0) else gcd(b%a,a)
 def solution(w,h): return w*h-w-h+gcd(w,h)
+
+# 다른 사람 풀이
+from math import gcd
+def solution(w,h):
+    return w * h - (w/gcd(w, h) + h/gcd(w, h) - 1) * gcd(w, h)
