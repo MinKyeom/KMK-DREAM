2f3819ab8a80c75d60e1f84e0e36b01b7764466f
minkyeom <minkyeom@naver.com>
2023-11-26 16:41:11 +0900

    algorism_review

--- Programmers/lv2/문자열 압축.py
+++ Programmers/lv2/문자열 압축.py
@@ -1,22 +1,27 @@
-# 내 풀이(개선 중)
+# 내 풀이
 def solution(s):
     result = s
-    k = 0
-    for a in range(1, int(len(s) / 2) + 1):
+
+    if len(s) % 2 == 1:
+        k = int(len(s) / 2) + 1
+    else:
+        k = int(len(s) / 2) + 1
+
+    for a in range(1, k):
         check = ""
-        before = s[0:a]
         count = 1
+        before = s[0:a]
         flag = False
+
+        #         if 2*a>=len(s):
+        #             continue
 
         for b in range(a, len(s), a):
             if b + a > len(s):
                 if count >= 2:
                     check = check + str(count) + before + s[b:]
-                else:
-                    check = check + s[b:]
-
-                falg = True
-                break
+                    flag = True
+                    break
 
             after = s[b:b + a]
 
@@ -35,14 +40,43 @@ def solution(s):
 
         if flag == False:
             if count >= 2:
-                check = check + str(count) + after
+                check = check + str(count) + before
             else:
-                check = check + after
+                check = check + before
 
-        # print(check)
-
+        # print(check,flag,len(check),a)
         if len(check) < len(result):
             result = check
-            k = a
-    print(k, result, flag)
+
     return len(result)
+
+# 다른 사람 풀이
+def compress(text, tok_len):
+    words = [text[i:i+tok_len] for i in range(0, len(text), tok_len)]
+    res = []
+    cur_word = words[0]
+    cur_cnt = 1
+    for a, b in zip(words, words[1:] + ['']):
+        if a == b:
+            cur_cnt += 1
+        else:
+            res.append([cur_word, cur_cnt])
+            cur_word = b
+            cur_cnt = 1
+    return sum(len(word) + (len(str(cnt)) if cnt > 1 else 0) for word, cnt in res)
+
+def solution(text):
+    return min(compress(text, tok_len) for tok_len in list(range(1, int(len(text)/2) + 1)) + [len(text)])
+
+a = [
+    "aabbaccc",
+    "ababcdcdababcdcd",
+    "abcabcdede",
+    "abcabcabcabcdededededede",
+    "xababcdcdababcdcd",
+
+    'aaaaaa',
+]
+
+for x in a:
+    print(solution(x))
