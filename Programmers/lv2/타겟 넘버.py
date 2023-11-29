292d359caca8faed897a16a5965f1f12c85d7303
minkyeom <minkyeom@naver.com>
2023-11-29 16:02:51 +0900

    algorism_review

---
+++ Programmers/lv2/타겟 넘버.py
@@ -0,0 +1,39 @@
+# 내 풀이
+def cal(check, a):
+    new = []
+
+    if len(check) == 0:
+        new.append(a)
+        new.append(-a)
+
+    else:
+        for k in check:
+            t_1 = k + a
+            t_2 = k - a
+            new.append(t_1)
+            new.append(t_2)
+
+    return new
+
+
+def solution(numbers, target):
+    answer = 0
+    check = []
+
+    for a in numbers:
+        check = cal(check, a)
+
+    for b in check:
+        if b == target:
+            answer += 1
+
+    return answer
+
+# 다른 사람 풀이
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
