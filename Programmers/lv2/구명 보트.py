d5aa20967a84bf017530c1afe19c41af36742e12
minkyeom <minkyeom@naver.com>
2023-11-30 15:48:19 +0900

    algorism_review

---
+++ Programmers/lv2/구명 보트.py
@@ -0,0 +1,37 @@
+# 내 풀이
+def solution(people, limit):
+    from collections import deque
+
+    answer = 0
+
+    people.sort(reverse=True)
+    people = deque(people)
+
+    while people:
+        a = people.popleft()
+
+        if len(people) > 0:
+            b = people.pop()
+        else:
+            b = 0
+
+        if a + b > limit:
+            people.append(b)
+
+        answer += 1
+
+    return answer
+
+# 다른 사람 풀이
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
