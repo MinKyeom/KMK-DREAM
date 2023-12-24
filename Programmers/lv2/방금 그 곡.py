255094a45a4897239c0a6c9d851b6678af8d3aa5
minkyeom <minkyeom@naver.com>
2023-12-24 13:58:12 +0900

    algorism_review

--- Programmers/lv2/방금 그 곡.py
+++ Programmers/lv2/방금 그 곡.py
@@ -1,10 +1,31 @@
-# 내 풀이(개선 중)
+# 내 풀이
 def solution(m, musicinfos):
     from collections import deque
 
     mu = musicinfos
 
     result = []
+
+    # 멜로디 모양 변형
+
+    melody_check = deque(list(m))
+
+    new_mel = []
+
+    while melody_check:
+        a = melody_check.popleft()
+
+        if a == "#":
+            b = new_mel.pop()
+            b = b.lower()
+            c = b + a
+            new_mel.append(c)
+        else:
+            new_mel.append(a)
+
+    m = "".join(new_mel)
+
+    # 주어진 가사 변형
 
     for k in mu:
         t = k.split(",")
@@ -17,44 +38,46 @@ def solution(m, musicinfos):
 
         run_time = time_finish - time_start  # 시작 할 때 첫 소절 나옴
 
-        if len(t[3]) > run_time:
-            pass
+        check_mel = deque(list(t[3]))
+        new_check = []
+
+        # 비교할 가사 모양 변형
+        while check_mel:
+            i = check_mel.popleft()
+
+            if i == "#":
+                j = new_check.pop()
+                j = j.lower()
+                h = j + i
+                new_check.append(h)
+            else:
+                new_check.append(i)
+
+        # print(new_check)
+
+        if len(new_check) > run_time:
+            new_check = new_check[0:run_time]
+
+        elif len(new_check) == run_time:
+            new_check = new_check
 
         else:
             count = 0
-            num = len(t[3])
-            while len(t[3]) <= run_time:
+            num = len(new_check)
+            while len(new_check) <= run_time:
                 if count == 0:
-                    t[3] += t[3][0]
+                    new_check += [new_check[0]]
                 else:
                     k = count % num
-                    t[3] += t[3][k]
+                    new_check += [new_check[k]]
                 count += 1
 
-        # print(t[3])
+        t[3] = "".join(new_check)
         t += [run_time]
 
-        # print(t[3],run_time)
-
         if m in t[3]:
-            if m[-1] != "#":
-                if (m + "#") in t[3]:
-                    continue
-                else:
-                    result.append(t)
-            else:
-                result.append(t)
+            result.append(t)
 
-        # elif t[3] in m:
-        #     if t[3][-1]!="#":
-        #         if t[3]+"#" in m:
-        #             continue
-        #         else:
-        #             result.append(t)
-        #     else:
-        else:
-            continue
-    # print(t[3][-1],"t[3]")
     if len(result) == 0:
         return "(None)"
 
@@ -63,3 +86,20 @@ def solution(m, musicinfos):
     return result[0][2]
 
 # 다른 사람 풀이
+
+def shap_to_lower(s):
+    s = s.replace('C#','c').replace('D#','d').replace('F#','f').replace('G#','g').replace('A#','a')
+    return s
+
+def solution(m,musicinfos):
+    answer=[0,'(None)']   # time_len, title
+    m = shap_to_lower(m)
+    for info in musicinfos:
+        split_info = info.split(',')
+        time_length = (int(split_info[1][:2])-int(split_info[0][:2]))*60+int(split_info[1][-2:])-int(split_info[0][-2:])
+        title = split_info[2]
+        part_notes = shap_to_lower(split_info[-1])
+        full_notes = part_notes*(time_length//len(part_notes))+part_notes[:time_length%len(part_notes)]
+        if m in full_notes and time_length>answer[0]:
+            answer=[time_length,title]
+    return answer[-1]
