bb70eaa25a44d9f67ce3d5338db675071666eef5
minkyeom <minkyeom@naver.com>
2024-01-14 16:51:49 +0900

    algorism_review

--- Programmers/lv2/하노이 탑.py
+++ Programmers/lv2/하노이 탑.py
@@ -1,29 +1,83 @@
 # 내 풀이(개선 중)
 from collections import deque
-
-
 def solution(n):
-    top_1 = deque([k for k in range(n, 0, -1)])
+    result = []
+    top_1 = deque([k for k in range(1, n + 1)])
     top_2 = deque([])
     top_3 = deque([])
-    count = 0
-    result = 0
-    print(top_1)
-    flag = False
 
-    #     while len(top_3)<=n:
-    #         # 큰 거 옮겨주기
-    #         if len(top_2)==0 or len(top_3)==0:
-    #             a=top_1.popleft()
-    #             if len(top_2)==0:
-    #                 top_2.append(a)
-    #             else:
-    #                 top_3.append(a)
+    if n == 1:
+        return [1, 3]
 
-    #             result+=1
-    #         # 합치기
-    #         else:
-    #             if top_3[-1]>top_2[-1]:
-    #                 b=top_2.pop()
+    elif n == 2:
+        return [[1, 2], [1, 3], [2, 3]]
+
+    else:
+        if n % 2 == 1:
+            a = top_1.popleft()
+            top_3.append(a)
+            result.append([1, 3])
+        else:
+            b = top_1.popleft()
+            top_2.append(b)
+            result.append([1, 2])
+
+        while len(top_3) <= n:
+            if len(top_2) == 0 or len(top_3) == 0:
+                t = top_1.popleft()
+                if len(top_2) == 0:
+                    top_2.append(t)
+                    result.append([1, 2])
+                else:
+                    top_3.append(t)
+                    result.append([1, 3])
+
+            else:
+                if len(top_2) == len(top_3) == 1:
+                    if top_2[-1] < top_3[-1]:
+                        c = top_2.pop()
+                        top3.append(c)
+                        result.append([2, 3])
+                    else:
+                        d = top_3.pop()
+                        top2.append(c)
+                        result.append([3, 2])
+
+                else:
+                    if len(top_2) > len(top_3):
+                        sub = []
+                        while top_2:
+                            e = top_2.pop()
+
+                            if len(sub) == 0:
+                                sub.append(e)
+                                result.append([2, 1])
+                            elif sub[-1] < e and top_3[-1] > e:
+                                top_3.append(e)
+                                result.append([2, 3])
+                            elif sub[-1] < e and top_3[-1] < e:
+                                s = sub.pop()
+                                top_3.append(s)
+                                result.append([1, 3])
+                                sub.append(e)
+                                result.append([])
 
     return result
+
+# 다른 사람 풀이
+
+def solution(n):
+    answer = hanoi(n, 1, 3, 2)
+    return answer
+
+
+def hanoi(n, start, end, bypass):
+    if n == 1:
+        return [[start, end]]
+    else:
+        path = []
+        path += hanoi(n - 1, start, bypass, end)
+        path += [[start, end]]
+        path += hanoi(n - 1, bypass, end, start)
+
+        return path
