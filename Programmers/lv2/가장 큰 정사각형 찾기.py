e74466aea3c68ac915819e836aef7b0f35f5405b
minkyeom <minkyeom@naver.com>
2024-01-19 16:29:19 +0900

    algorism_review

--- Programmers/lv2/가장 큰 정사각형 찾기.py
+++ Programmers/lv2/가장 큰 정사각형 찾기.py
@@ -101,7 +101,6 @@ def solution(board):
 """
 
 # 다른 사람 풀이
-# 다른 사람 풀이
 
 def solution(board):
     answer = board[0][0]
@@ -113,3 +112,23 @@ def solution(board):
                 answer = max(answer, board[i][j])
 
     return answer ** 2
+
+
+def solution(board):
+    n = len(board)
+    m = len(board[0])
+
+    # 2중 포문으로 연산
+    for i in range(1, n):
+        for j in range(1, m):
+            if board[i][j] == 1:
+                board[i][j] = min(board[i - 1][j - 1], board[i - 1][j], dp[i][j - 1]) + 1
+
+    # 최대 넓이 구하기
+    answer = 0
+    for i in range(n):
+        temp = max(board[i])
+        answer = max(answer, temp)
+
+    return answer ** 2
+
