5bf361b0d25f19f8f486792993bb3cffda4d7435
minkyeom <minkyeom@naver.com>
2024-01-24 15:43:07 +0900

    algorism_review

--- Programmers/lv2/124나라의 숫자.py
+++ Programmers/lv2/124나라의 숫자.py
@@ -1,32 +1,50 @@
-# 내 풀이(개선 중)
+# 내 풀이
+# 1.재귀로 연속으로 숫자를 바꾸기
+# 2.조합으로 생각 후 연속 된 숫자를 나눠서 위치로 숫자 찾기
 # 1.재귀로 연속으로 숫자를 바꾸기
 # 2.조합으로 생각 후 연속 된 숫자를 나눠서 위치로 숫자 찾기
 def solution(n):
-    num = ["1", "2", "4"]
+    k = ["1", "2", "4"]
     result = ""
+    num = n
+    count = 1  # 자릿수 파악
 
-    n = 6
+    while num > 0:
+        num -= 3 ** (count)
 
-    if n == 1:
-        return "1"
-    elif n == 2:
-        return "2"
-    elif n == 3:
-        return "4"
+        if num > 0:
+            count += 1
 
-    while True:
-        i, j = divmod(n, 3)
+    for a in range(1, count):
+        n -= 3 ** a
 
-        if i == 0:
-            result += num[j - 1]
-            break
-        elif j == 0:
-            result += num[-1]
-            n = j
+    while count > 1:
+        i, j = divmod(n, 3 ** (count - 1))
+        if j == 0:
+            result += k[i - 1]
         else:
-            result += num[i - 1]
-            n = j
+            result += k[i]
+        n = n % (3 ** (count - 1))
+        count -= 1
+
+    if count == 1:
+        i, j = divmod(n - 1, 3)
+        result += k[j]
 
     return result
 
 # 다른 사람 풀이
+def change124(n):
+    num = ['1','2','4']
+    answer = ""
+
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print(change124(10))
