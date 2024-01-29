eccdb88effd97641b39ef04a9043f356c6ae8798
minkyeom <minkyeom@naver.com>
2024-01-29 14:25:22 +0900

    algorism_review

--- Programmers/lv3/주사위 고르기.py
+++ Programmers/lv3/주사위 고르기.py
@@ -251,3 +251,38 @@ def solution(dice):
     return result
 # 다른 사람 풀이
 
+from itertools import combinations, product
+from bisect import bisect_left
+
+
+def solution(dice):
+    dices = dice
+    dice_indexes = range(len(dices))
+    result = []
+    for first_comb in combinations(dice_indexes, len(dice_indexes) // 2):
+        first_dice_indexes = set(first_comb)
+        second_dice_indexes = set(dice_indexes) - first_dice_indexes
+        first_point_set = calcPointSet([dices[i] for i in first_dice_indexes])
+        second_point_set = calcPointSet([dices[i] for i in second_dice_indexes])
+
+        first_wins = 0
+        for first_point in first_point_set:
+            first_wins += bisect_left(second_point_set, first_point)
+
+        result.append((first_comb, first_wins / 6 ** len(dice_indexes)))
+
+    most_win_dice_indexes = list(sorted(result, key=lambda x: x[1], reverse=True))[0][0]
+
+    return list(map(lambda x: x + 1, most_win_dice_indexes))
+
+
+def calcPointSet(dices):
+    point_set = []
+    for _comb in product(*[range(6) for _ in range(len(dices))]):
+        _sum = 0
+        for index, dice in zip(_comb, dices):
+            _sum += dice[index]
+        point_set.append(_sum)
+
+    return sorted(point_set)
+
