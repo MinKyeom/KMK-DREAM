c2dbea18fed223e0c5cdc730fccbd99d3bc1b4d4
minkyeom <minkyeom@naver.com>
2024-02-01 15:45:43 +0900

    algorism_review

--- Programmers/lv2/도넛과 막대 그래프.py
+++ Programmers/lv2/도넛과 막대 그래프.py
@@ -108,3 +108,85 @@ def solution(edges):
         num = deque(list(set(num) - set(visit)))
 
     return result
+
+# 내 풀이 (개선중)
+# 정점을 없애고 그래프 모양을 판별 후 갯수 추가
+# 정점을 제외한 부분에서 나머지 점에서 출발 후 모양 판별
+from collections import deque
+
+
+def solution(edges):
+    result = [0] * 4
+    map = {}  # 단방향성 그래프
+    g = {}  # 양방향 연결 그래프 체크
+    num = []  # 정점모음
+    arrive = []
+
+    for a, b in edges:
+        if not a in map:
+            map[a] = [b]
+        else:
+            map[a].append(b)
+        if not b in map:
+            map[b] = []
+        if not a in g:
+            g[a] = [b]
+        else:
+            g[a].append(b)
+        if not b in g:
+            g[b] = [a]
+        else:
+            g[b].append(a)
+
+        num.append(a)
+        num.append(b)
+        arrive.append(b)
+
+    num = list(set(num))
+
+    # 정점 특징:도착하는 점에 없음,최소 2개의 그래프를 이어서 두 개의 간선 존재
+    stop_check = list(set(num) - set(arrive))
+
+    # 정점 찾기
+    for c in stop_check:
+        if len(map[c]) >= 2:
+            stop = c
+            result[0] = stop
+            break
+
+    graph_check = []
+    graph_check += map[stop]
+
+    del map[stop]  # 정점제거
+    del g[stop]
+
+    num = list(set(num) - set([stop]))
+    num = deque(num)
+
+    graph_check = deque(graph_check)
+
+    while graph_check:
+        t = graph_check.popleft()
+        q = deque([t])
+        visit = []
+        line = 0
+
+        # 연결된 정점 확인
+        while q:
+            n = q.popleft()
+            if not n in visit and n != stop:
+                visit.append(n)
+                line += len(map[n])
+                for i in g[n]:
+                    if not i in visit and i != stop:
+                        q.append(i)
+
+        # 연결된 모양 판별
+        if line == len(visit) + 1:
+            result[3] += 1
+        elif line == len(visit):
+            result[1] += 1
+        else:
+            result[2] += 1
+
+    return result
