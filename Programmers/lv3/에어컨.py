7c7dc858dd301e0527707c6dbf79bb4e1b910f94
minkyeom <minkyeom@naver.com>
2024-02-18 09:50:26 +0900

    algorism_review

--- Programmers/lv3/에어컨.py
+++ Programmers/lv3/에어컨.py
@@ -1,4 +1,68 @@
 # 내 풀이(개선 중)
+from collections import deque
+
+
+def solution(temperature, t1, t2, a, b, onboard):
+    result = 0  # 전력소모량
+    p = onboard  # 승객 탑승 여부
+    t = temperature  # temperature: 실외온도
+    now = temperature  # 현재기온
+    # want: 희망온도 off: 전원 temperature: 시간에 따른 온도
+    check = [[now, temperature, "off"] for _ in range(len(p))]  # 시간에 따른 에어컨 전원 및 온도 변경여부 0,1,-1
+
+    #### 목표 ####
+    # 승객이 탑승중일때 최적화된 실내 온도를 최소의 전력으로 맞추기
+    # 승객이 탑승할때도 에어컨이 켜져있다면 외부 온도와 관계없이 에어컨의 조건에 따른다!
+    # 사이인거 등호 포함!
+    # 더 더울경우 t2 추울경우 t1
+    # 탑승객이 없을경우 최적화를 맞출 필요가 없다 그래야 최소 전력 가능!
+    # 변화가 생기기 직전에 에어컨 on
+    #################################
+
+    # 기준점 t: 현재 온도
+
+    ####
+    # 온도를 선택하는 방법 중 전력이 최소로 드는걸 선택
+    # [현재온도, 전력]으로 이루어진 리스트를 활용하여 체크
+
+    count = 0
+    # [현재기온,전력,시간]
+    q = deque([[now, 0, 0]])
+    result = []
+
+    while q:
+        c, e, time = q.popleft()
+
+        # 최적화 온도 여부 체크
+        if not t1 <= c <= t2 and p[time] == 1:
+            continue
+
+        if time == len(p) - 1:
+            result.append(e)
+            continue
+
+        # 에어컨 off 경우
+        if c == t:
+            q.append([c, e, time + 1])
+        elif c > t:
+            q.append([c - 1, e, time + 1])
+        elif c < t:
+            q.append([c + 1, e, time + 1])
+
+        # 에어컨 on 경우 변동
+        if c > t2:
+            q.append([c - 1, e + a, time + 1])
+        elif c < t1:
+            q.append([c + 1, e + a, time + 1])
+        elif t1 <= c <= t2:
+            q.append([c, e + b, time + 1])
+            q.append([c + 1, e + a, time + 1])
+            q.append([c - 1, e + a, time + 1])
+
+    return min(result)
+
+
+# 내 풀이(개선 중)
 def solution(temperature, t1, t2, a, b, onboard):
     result = 0  # 전력소모량
     p = onboard  # 승객 탑승 여부
