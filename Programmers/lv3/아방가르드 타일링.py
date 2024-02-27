2bafb4eaff3a5ee51c11b1b9816a11cddcbb4b60
minkyeom <minkyeom@naver.com>
2024-02-27 18:01:03 +0900

    algorism_review

--- Programmers/lv3/아방가르드 타일링.py
+++ Programmers/lv3/아방가르드 타일링.py
@@ -1,3 +1,44 @@
+# 내 풀이(개선 중3)
+def solution(n):
+    # n:n가로 길이
+    dp = [0, 1, 3, 10]
+    check = [2, 2, 4]
+    if n > 6:
+        dp += [0] * (n - 3)
+        dp[4] = dp[3] + dp[2] * 2 + dp[1] * 5 + 2
+        dp[5] = dp[4] + dp[3] * 2 + dp[2] * 5 + dp[1] * 2 + 2
+        dp[6] = dp[5] + dp[4] * 2 + dp[3] * 5 + dp[2] * 2 + dp[1] * 2 + 4
+
+    else:
+        dp += [0] * 3
+        dp[4] = dp[3] + dp[2] * 2 + dp[1] * 5 + 2
+        dp[5] = dp[4] + dp[3] * 2 + dp[2] * 5 + dp[1] * 2 + 2
+        dp[6] = dp[5] + dp[4] * 2 + dp[3] * 5 + dp[2] * 2 + dp[1] * 2 + 4
+        return dp[n] % 1000000007
+
+    for i in range(7, n + 1):
+        result = 0
+        k = i - 1
+        count = 0
+        while k >= 0:
+            if k == i - 1:
+                result += dp[k]
+            elif k == i - 2:
+                result += 2 * dp[k]
+            elif k == i - 3:
+                result += 5 * dp[k]
+            elif k == 0:
+                num = count % 3
+                result += check[num]
+            else:
+                num = count % 3
+                result += (check[num] * dp[k])
+                count += 1
+            k -= 1
+
+        dp[i] = result % 1000000007
+
+    return dp[n]  # %1000000007
 # 내 풀이(개선 중2)
 def solution(n):
     # n:n가로 길이
