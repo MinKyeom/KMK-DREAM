841ece6bce5f18b7379252b651d4a1861a7abc73
minkyeom <minkyeom@naver.com>
2024-03-11 21:27:20 +0900

    algorism_review

---
+++ Programmers/lv3/미로 탈출 명령어.py
@@ -0,0 +1,64 @@
+# 내 풀이(개선 중)
+# 같은 곳을 두 번 방문할 수 있다
+# 거리가 k로 고정
+# bfs 같은 곳 방문 여러번 가능 단 거리는 k여야 한다
+# dx dy [1,0]은 아래로 가는 것이다 위x 격자 미로에서 상상해야 한다
+
+# 시간 초과 발생: 길이가 k가 되는 것들을 모아 다시 도착 지점에 있는지 확인 하는 방법 생각해보고 접근해보기!
+from collections import deque
+
+
+def solution(n, m, x, y, r, c, k):
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+
+    # s=[x,y] # 출발 지점
+    # d=[r,c] # 도착 지점
+
+    result = []
+
+    q = deque([["", [x, y]]])
+
+    while q:
+        i = q.popleft()
+        # print(i,"len(I)",len(i),"[r,c]",[r,c])
+        if abs(i[-1][0] - r) + abs(i[-1][1] - c) + len(i[0]) > k:
+            continue
+        if len(i[0]) == k and i[-1] == [r, c]:
+            i.pop()
+            # print(i)
+            # i.sort()
+            # final="".join(i)
+            # print(final)
+            # result.append(final)
+            result.append(i[0])
+        elif len(i[0]) == k and i[-1] != [r, c]:
+            continue
+
+        else:
+            v = i[-1][0]
+            w = i[-1][1]
+            i.pop()  # 맨 뒤 리스트 제거
+            for a, b in zip(dx, dy):
+                new = ""
+                if 1 <= v + a <= n and 1 <= w + b <= m:
+                    if a == 1 and b == 0:
+                        new += str(i[0])
+                        new += "d"
+                        q += [[new, [v + a, w + b]]]
+                    elif a == -1 and b == 0:
+                        new += str(i[0])
+                        new += "u"
+                        q += [[new, [v + a, w + b]]]
+                    elif a == 0 and b == 1:
+                        new += str(i[0])
+                        new += "r"
+                        q += [[new, [v + a, w + b]]]
+                    elif a == 0 and b == -1:
+                        new += str(i[0])
+                        new += "l"
+                        q += [[new, [v + a, w + b]]]
+
+    result.sort()
+
+    return result[0] if len(result) > 0 else "impossible"
