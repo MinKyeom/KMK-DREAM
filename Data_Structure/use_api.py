657fd587c9df16f4f349736b25def02a4f7b8367
minkyeom <minkyeom@naver.com>
2024-03-29 17:27:51 +0900

    Data_Structure

--- Data_Structure/use_api.py
+++ Data_Structure/use_api.py
@@ -13,7 +13,29 @@ d2=json.loads(d_convert)
 
 print(type(d2))
 
-# 괄호에 따른 기본 변환 과정 다시 체크 
-d3=json.loads("{'name': 'min' }")
+# 괄호에 따른 기본 변환 과정 다시 체크
+d3=json.loads('{"name": "min" }')
 
 print(d3)
+
+import pandas as pd
+
+d4_str = """
+[
+  {"name": "혼자 공부하는 데이터 분석", "author": "박해선", "year": 2022},
+  {"name": "혼자 공부하는 머신러닝+딥러닝", "author": "박해선", "year": 2020}
+]
+"""
+d4 = json.loads(d4_str)
+
+
+import pandas as pd
+import io
+d4=io.StringIO(d4_str)
+
+pd.read_json((d4))
+print(d4,"new")
+pd.DataFrame(d4)
+print(d4)
+
+
