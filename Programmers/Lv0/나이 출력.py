d9159d5041ac28ef2de0359254e940762b294c79
minkyeom <minkyeom@naver.com>
2024-04-14 22:06:56 +0900

    algorism_review,categorize

--- Programmers/Lv0/나이 출력.py
+++ Programmers/Lv0/나이 출력.py
@@ -1,3 +1,7 @@
+"""
+출처: 프로그래머스 코딩 테스트 연습,
+https://school.programmers.co.kr/learn/courses/30/lessons/120820
+"""
 # 내 풀이
 def solution(age):
     return (2022 - age + 1)
--- Programmers/Lv0/다음에 올 숫자.py
+++ Programmers/Lv0/다음에 올 숫자.py
@@ -1,3 +1,7 @@
+"""
+출처: 프로그래머스 코딩 테스트 연습,
+https://school.programmers.co.kr/learn/courses/30/lessons/120924
+"""
 #1 풀이 (정답 나옴!)
 """
 
