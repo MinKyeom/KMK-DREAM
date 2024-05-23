e5d9a61e51f2c8fed2ab4fdd853b1824b1e46383
minkyeom <minkyeom@naver.com>
2024-05-23 16:18:41 +0900

    algorism_review

--- Programmers/lv3/양과 늑대.py
+++ Programmers/lv3/양과 늑대.py
@@ -115,3 +115,37 @@ def solution(info, edges):
     sheep = 0
 
     return 0
+
+# 다른 사람 풀이
+
+from collections import defaultdict
+
+answer = -1
+
+
+def solution(info, edges):
+    graph = defaultdict(list)
+    for u, v in edges:
+        graph[u].append(v)
+
+    def dfs(cur, sheep, wolf, next_set):
+        sheep += info[cur] ^ 1
+        wolf += info[cur]
+
+        if sheep <= wolf:
+            return
+        if sheep > wolf:
+            global answer
+            answer = max(answer, sheep)
+            for next_ in next_set:
+                temp = set(graph.get(next_, []))
+                next_set |= temp
+                next_set -= set([next_])
+                dfs(next_, sheep, wolf, next_set)
+                next_set |= set([next_])
+                next_set -= temp
+
+
+    dfs(0, 0, 0, set(graph.get(0)))
+    print(answer)
+    return answer
