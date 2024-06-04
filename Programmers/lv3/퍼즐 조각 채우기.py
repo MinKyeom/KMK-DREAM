2e20fe4b96cbb72cec3ababa59cc5fe6d76d9f18
minkyeom <minkyeom@naver.com>
2024-06-04 20:44:04 +0900

    algorism_review

---
+++ Programmers/lv3/퍼즐 조각 채우기.py
@@ -0,0 +1,27 @@
+"""
+출처:프로그래머스
+https://school.programmers.co.kr/learn/courses/30/lessons/84021
+"""
+
+# 내 풀이(개선 중)
+"""
+조건:
+1회에 1번
+조각 회전가능
+뒤집기 불가능 
+
+목표:
+채울 수 있는 최대칸을 구하여라 
+
+생각방향:모양이 들어맞아야 인접한 곳이 비는 일이 없어짐에 따라 빈 곳과 테이블을 비교해야한다
+
+회전하는 방법에 대한 아이디어 생각
+
+"""
+
+
+def solution(game_board, table):
+    answer = -1
+    return answer
+
+
