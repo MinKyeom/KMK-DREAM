b0be17333c9046b6974a358db17b1025c496cbda
minkyeom <minkyeom@naver.com>
2024-07-17 17:11:15 +0900

    algorism_review

--- Programmers/lv3/스타 수열.py
+++ Programmers/lv3/스타 수열.py
@@ -5,6 +5,62 @@ https://school.programmers.co.kr/learn/courses/30/lessons/70130
 # 내 풀이(개선 중)
 """
 생각방향
+# 부분수열의 모든 경우의 수를 비트마스크로 나타낸 후 스타수열 거르기
+"""
+from itertools import combinations
+
+def solution(a):
+    k = []
+    n = len(a) // 2
+    result = 0
+
+    bit_mask = []
+
+    # 부분 수열 만들기 & 1번 조건
+    for i in range(1 << len(a)):
+        sub = []
+        for j in range(len(a)):
+            if i & (1 << j):
+                sub.append(a[j])
+        bit_mask.append(sub)
+
+    # bit_mask=sorted(bit_mask, key=lambda x:(len(x)))
+
+    # 스타수열 찾기
+    while bit_mask:
+        check = bit_mask.pop()
+
+        if len(check) % 2 == 1 or len(check) < 2:
+            continue
+
+        elif len(check) <= result:
+            continue
+
+        elif len(check) == 2:
+            if len(set(check)) == 1:
+                continue
+            result = max(result, len(check))
+            continue
+
+        # 2번 조건 확인
+        common = set([check[0], check[1]]) & set([check[2], check[3]])
+
+        if len(common) == 0:
+            continue
+
+        else:
+            for s in range(0, len(check), 2):
+                common = set([check[s], check[s + 1]]) & common
+                if len(common) == 0 or check[s] == check[s + 1]:
+                    break
+            else:
+                result = max(result, len(check))
+
+    return result
+
+# 내 풀이(개선 중)
+"""
+생각방향
 """
 from itertools import combinations
 
