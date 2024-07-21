ad9e5902b4c38cb254ec166673f255b55474628a
minkyeom <minkyeom@naver.com>
2024-07-21 15:34:05 +0900

    algorism_review

---
+++ Programmers/lv3/풍선 터뜨리기.py
@@ -0,0 +1,32 @@
+"""
+출처: 프로그래머스
+https://school.programmers.co.kr/learn/courses/30/lessons/68646
+"""
+# 내 풀이(개선 중)
+"""
+맨 끝에 두 개는 항상 살아남음 
+
+"""
+def solution(a):
+    result = []
+
+    if len(a) >= 3:
+        for i in range(1, len(a) - 1):
+            num = a[i]
+            left = float("inf")
+            right = float("inf")
+
+            # 좌
+            for l in range(i):
+                left = min(left, a[l])
+
+            # 우
+            for r in range(i + 1, len(a)):
+                right = min(right, a[r])
+
+            if right < num and left < num:
+                continue
+            else:
+                result.append(num)
+
+    return len(set(result)) + 2
