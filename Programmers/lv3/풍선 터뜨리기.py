4df400b3290037a5476a600fc9a750dd9ef228e4
minkyeom <minkyeom@naver.com>
2024-07-22 15:55:09 +0900

    algorism_review

--- Programmers/lv3/풍선 터뜨리기.py
+++ Programmers/lv3/풍선 터뜨리기.py
@@ -13,6 +13,42 @@ def solution(a):
     if len(a) >= 3:
         for i in range(1, len(a) - 1):
             num = a[i]
+
+            # 좌
+            count_left = 0
+            for l in range(i):
+                if a[l] < num:
+                    count_left += 1
+                    if count_left >= 2:
+                        break
+            # 우
+            count_right = 0
+            for r in range(i + 1, len(a)):
+                if a[r] < num:
+                    count_right += 1
+                    if count_right >= 2:
+                        break
+
+            if count_left == 0 and count_right >= 1:
+                result.append(num)
+            elif count_right == 0 and count_left >= 1:
+                result.append(num)
+            elif count_right == 0 and count_left == 0:
+                result.append(num)
+
+    return len(set(result)) + 2
+
+# 내 풀이(개선 중)
+"""
+맨 끝에 두 개는 항상 살아남음 
+
+"""
+def solution(a):
+    result = []
+
+    if len(a) >= 3:
+        for i in range(1, len(a) - 1):
+            num = a[i]
             left = float("inf")
             right = float("inf")
 
