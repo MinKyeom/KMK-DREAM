cbbf8200c6cae341886d33475af5422f503ad09d
minkyeom <minkyeom@naver.com>
2024-07-24 08:02:53 +0900

    algorism_review

--- Programmers/lv3/풍선 터뜨리기.py
+++ Programmers/lv3/풍선 터뜨리기.py
@@ -79,7 +79,7 @@ def solution(a):
     # 우측->좌측 방향 최솟값dp
     for i in range(len(a) - 2, -1, -1):
         dp[1][i] = min(dp[1][i + 1], a[i])
-ㄴ
+
     for i in range(1, len(a) - 1):
         left_min = dp[0][i - 1]  # 좌측 최솟값
         right_min = dp[1][i + 1]  # 우측 최솟값
@@ -89,6 +89,26 @@ def solution(a):
         # 한쪽은 크고 다른한쪽은 작은경우
         elif (left_min > a[i] and right_min < a[i])
             or (left_min < a[i] and right_min > a[i]):
-        answer += 1
+            answer += 1
 
     return answer
+
+
+def solution(a):
+    ret = 2
+    if 0 <= len(a) <= 2:
+        return len(a)
+    left = a[0]
+    right = a[-1]
+
+    for i in range(1, len(a) - 1):
+        if left > a[i]:
+            left = a[i]
+            ret += 1
+        if right > a[-1 - i]:
+            right = a[-1 - i]
+            ret += 1
+    if left == right:
+        return ret - 1
+    else:
+        return ret
