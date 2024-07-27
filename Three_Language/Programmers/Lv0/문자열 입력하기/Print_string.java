011ae7d08cde23bc6392a994bb1f17f51f844b82
minkyeom <minkyeom@naver.com>
2024-07-27 18:47:18 +0900

    start_java

---
+++ Three_Language/Programmers/Lv0/문자열 입력하기/Print_string.java
@@ -0,0 +1,20 @@
+package lv0;
+import java.util.Scanner;
+
+public class Print_string {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		// TODO Auto-generated method stub
+		String str;
+		str=scanner.nextLine();
+		
+		System.out.println(str);
+		
+		
+				
+		
+
+	}
+
+}
