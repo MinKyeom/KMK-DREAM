ce20333b3692f35111dfacd2237f9b40a4122930
minkyeom <minkyeom@naver.com>
2024-08-25 21:23:17 +0900

    algorism_review

--- Programmers/lv3/자물쇠와 열쇠.py
+++ Programmers/lv3/자물쇠와 열쇠.py
@@ -36,6 +36,85 @@ def rotation(k, r, n):
 
 def solution(key, lock):
     # rotation=[0,1,2,3]
+    n = len(lock)
+    check = [k - n for k in range(2 * n + 1)]
+
+    move = list(product(check, repeat=2))
+
+    # print(move)
+
+    lock_check = []
+    lock_break = []
+    # 홈 위치,돌기 위치
+    for i in range(len(lock)):
+        for j in range(len(lock)):
+            # 홈
+            if lock[i][j] == 0:
+                lock_check.append((i, j))
+            # 돌기
+            else:
+                lock_break.append((i, j))
+
+    key_check = []
+
+    for i in range(len(key)):
+        for j in range(len(key)):
+            if key[i][j] == 1:
+                key_check.append((i, j))
+
+    lock_check = set(lock_check)
+    lock_break = set(lock_break)
+    key_check = set(key_check)
+
+    # 0 90 180 270
+    for r in range(5):
+        check_open = rotation(key_check, r, len(key))
+
+        for dx, dy in move:
+            new_move = set([])
+
+            for nx, ny in check_open:
+                new_move.add((nx + dx, ny + dy))
+            # print(new_move,dx,dy)
+
+            if len(lock_check - new_move) == 0 and len(new_move & lock_break) == 0:
+                return True
+
+    return False
+# 내 풀이(개선 중)
+"""
+최대 이동 상하좌우 모두 n번씩
+회전 n
+
+상하 , 좌우 
+각각 모두 한 후 
+회전 90도를 4번 한 후 확인 후 
+그래도 불가능하다면 False
+"""
+import copy
+from itertools import product
+
+
+def rotation(k, r, n):
+    new = set([])
+
+    for x, y in k:
+        for rota in range(r):
+            x, y = (n - 1 - x), x
+
+        new.add((x, y))
+
+    return new
+
+
+# def move():
+#     return 0
+
+# def open_door():
+#     return 0
+
+def solution(key, lock):
+    # rotation=[0,1,2,3]
     n = len(key)
     check = [k - n for k in range(2 * n + 1)]
 
