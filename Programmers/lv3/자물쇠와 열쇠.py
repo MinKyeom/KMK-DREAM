e5ad41c75833c37b16137f4caa9a59f49f927b40
minkyeom <minkyeom@naver.com>
2024-08-26 16:39:22 +0900

    algorism_review

--- Programmers/lv3/자물쇠와 열쇠.py
+++ Programmers/lv3/자물쇠와 열쇠.py
@@ -2,6 +2,87 @@
 출처:프로그래머스
 https://school.programmers.co.kr/learn/courses/30/lessons/60059
 """
+# 내 풀이
+"""
+최대 이동 상하좌우 모두 n번씩
+회전 n
+
+상하 , 좌우 
+각각 모두 한 후 
+회전 90도를 4번 한 후 확인 후 
+그래도 불가능하다면 False
+"""
+import copy
+from itertools import product
+
+
+def rotation(k, r, l):
+    new = set([])
+
+    for x, y in k:
+        for rota in range(r):
+            y, x = x, (l - 1 - x)
+
+        new.add((x, y))
+
+    return new
+
+
+# def move():
+#     return 0
+
+# def open_door():
+#     return 0
+
+def solution(key, lock):
+    # rotation=[0,1,2,3]
+    num = max(len(key), len(lock))
+    n = num
+    check = [k for k in range(-3 * n, 3 * n + 1)]
+
+    move = list(product(check, repeat=2))
+
+    lock_check = []
+    lock_break = []
+
+    # 홈 위치,돌기 위치
+    for i in range(len(lock)):
+        for j in range(len(lock)):
+            # 홈
+            if lock[i][j] == 0:
+                lock_check.append((i, j))
+            # 돌기
+            else:
+                lock_break.append((i, j))
+
+    key_check = []
+
+    for i in range(len(key)):
+        for j in range(len(key)):
+            if key[i][j] == 1:
+                key_check.append((i, j))
+
+    lock_check = set(lock_check)
+    lock_break = set(lock_break)
+    key_check = set(key_check)
+
+    # 0 90 180 270
+    for r in range(4):
+        check_open = rotation(key_check, r, len(key))
+
+        for dx, dy in move:
+            new_move = set([])
+
+            for nx, ny in check_open:
+                new_move.add((nx + dx, ny + dy))
+            # print(new_move,dx,dy)
+
+            # 자물쇠 홈-열쇠 돌기 and 열쇠 돌기 자물쇠 돌기
+            if len(lock_check - new_move) == 0 and len(new_move & lock_break) == 0:
+                return True
+
+    return False
+
 # 내 풀이(개선 중)
 """
 최대 이동 상하좌우 모두 n번씩
