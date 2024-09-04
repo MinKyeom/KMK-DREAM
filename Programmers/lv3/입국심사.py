4fe9e2bee1e218fd86f55b4bef5d72acac3d61e7
minkyeom <minkyeom@naver.com>
2024-09-04 17:39:18 +0900

    algorism_review

---
+++ Programmers/lv3/입국심사.py
@@ -0,0 +1,31 @@
+"""
+출처:프로그래머스
+https://school.programmers.co.kr/learn/courses/30/lessons/43238
+"""
+
+# 내 풀이
+def solution(n, times):
+    times.sort()
+
+    left = 0
+    right = n * max(times)
+
+    result = []
+
+    while left <= right:
+        count = 0
+        mid = (left + right) // 2
+
+        for t in times:
+            count += (mid // t)
+            if count > n:
+                break
+
+        if count >= n:
+            right = mid - 1
+            result.append(mid)
+
+        else:
+            left = mid + 1
+
+    return min(result)
