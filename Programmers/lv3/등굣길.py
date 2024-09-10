8350a2571e9168d0be1382a0cb7c069a2bc3f842
minkyeom <minkyeom@naver.com>
2024-09-10 15:50:46 +0900

    algorism_review

--- Programmers/lv3/등굣길.py
+++ Programmers/lv3/등굣길.py
@@ -31,3 +31,22 @@ def solution(m, n, puddles):
                 check[i][j] = 0
 
     return check[n][m] % 1000000007
+
+
+# 다른 사람 풀이
+def solution(m,n,puddles):
+    grid = [[0]*(m+1) for i in range(n+1)] #왼쪽, 위로 한줄씩 만들어서 IndexError 방지
+    if puddles != [[]]:                    #물이 잠긴 지역이 0일 수 있음
+        for a, b in puddles:
+            grid[b][a] = -1                #미리 -1로 체크
+    grid[1][1] = 1
+    for j in range(1,n+1):
+        for k in range(1,m+1):
+            if j == k == 1:                #(1,1)은 1로 만들어두고, 0이 되지 않도록
+                continue
+            if grid[j][k] == -1:           #웅덩이는 0으로 만들어 다음 덧셈 때 영향끼치지 않게
+                grid[j][k] = 0
+                continue
+            grid[j][k] = (grid[j][k-1] + grid[j-1][k])%1000000007   #[a,b] = [a-1,b] + [a,b-1] 공식
+
+    return grid[n][m]
--- Programmers/lv3/정수삼각형.py
+++ Programmers/lv3/정수삼각형.py
@@ -17,3 +17,16 @@ def solution(triangle):
             triangle[i][j] = max((triangle[i - 1][j - 1] + triangle[i][j]), (triangle[i - 1][j] + triangle[i][j]))
 
     return max(triangle[len(triangle) - 1])
+
+# 다른 사람 풀이
+def solution(triangle):
+    dp = []
+    for t in range(1, len(triangle)):
+        for i in range(t+1):
+            if i == 0:
+                triangle[t][0] += triangle[t-1][0]
+            elif i == t:
+                triangle[t][-1] += triangle[t-1][-1]
+            else:
+                triangle[t][i] += max(triangle[t-1][i-1], triangle[t-1][i])
+    return max(triangle[-1])
