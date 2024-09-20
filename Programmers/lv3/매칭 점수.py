581e70d1258a5e4cd771ca36c715cb1e813d28a2
minkyeom <minkyeom@naver.com>
2024-09-20 21:09:20 +0900

    algorism_review

--- Programmers/lv3/매칭 점수.py
+++ Programmers/lv3/매칭 점수.py
@@ -11,6 +11,123 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42893
 
 한 웹페이지의 링크점수는 해당 웹페이지로 링크가 걸린 다른 웹페이지의 기본점수 ÷ 외부 링크 수의 총합이다.
 """
+
+from collections import defaultdict
+from collections import deque
+import re
+
+
+def solution(word, pages):
+    new = []
+    word = word.upper()
+    english = set(list("abcdefghijklmnopqrstuvwxyz".upper()))
+    english = english | set(list("abcdefghijklmnopqrstuvwxyz"))
+    # 페이지 모두 대문자로 변환
+    for p in pages:
+        n = p.upper()
+        new.append(n)
+
+    # 기본점수, 외부 링크점수
+    check = [[0] * 2 for i in range(len(new))]
+
+    # 웹페이지별 구분
+    linkpage = []
+
+    # 내부 링크 구분 및 링크 정리 > linkpage
+    for n in range(len(new)):
+        page = re.findall('<a href=\"https://([\S]+)\">', pages[n])
+        if page is None:
+            check[n][1] = 0
+        else:
+            check[n][1] = len(page)
+
+        # print(external_links)
+
+        # check[n][1]=new[n].count("<A HREF")
+        url = re.search('<meta property=\"og:url\" content=\"https://([\S]+)\".*/>', pages[n]).group(1)
+        linkpage.append(url)
+    """
+        l=new[n]
+        begin=""
+        for f in range(len(l)):
+            begin+=l[f]
+            if "OG:URL" in begin:
+                start=''
+                link_page=deque(list(l[f+2:]))
+                while link_page:
+                    k=link_page.popleft()
+                    if k=="\"":
+                        while True:
+                            s=link_page.popleft()
+                            if s=="|":
+                                break
+                            else:
+                                start+=s
+                            print(start,s)
+                linkpage.append(start)
+                break
+    """
+
+    # print(linkpage)
+    # 링크 점수
+    check_list = [0] * len(pages)
+
+    #     #기본 점수
+    #     for count,i in enumerate(new):
+    #         num=0
+    #         start=""
+    #         for eng in list(i):
+    #             if eng in [a-zA-Z]:
+    #                 start+=eng
+    #             else:
+    #                 if start.upper()==word.upper():
+    #                     num+=1
+    #                     start=""
+
+    #                 else:
+    #                     start=""
+    #         #print(num)
+    #         check[count][0]=num
+    for i in range(len(new)):
+        cnt = 0
+        for w in re.findall('[a-zA-Z]+', pages[i]):
+            if w.upper() == word:
+                cnt += 1
+        check[i][0] = cnt
+
+    # 링크 점수
+    for my in range(len(pages)):
+        for another in range(len(pages)):
+            if my != another and linkpage[another] in (pages[my]):
+                # print("또 다른 곳",linkpage[another],"check page",my,linkpage[my])
+                # 링크점수 해당 링크 기본점수 / 외부링크 수
+                if check[my][1] != 0:
+                    check_list[another] += check[my][0] / check[my][1]
+    # print(check_list)
+
+    result = []
+    # print(check,"기본 점수, 외부 링크 수")
+    # print(check_list,"링크 점수")
+
+    for r in range(len(check_list)):
+        result.append(check[r][0] + check_list[r])
+
+    # print(result)
+
+    # print(result,"결과")
+
+    return result.index(max(result))
+
+
+# 내 풀이(개선 중)
+"""
+조건:
+기본점수: 검색어가 등장하는 횟수
+외부링크 수:다른 외부 페이지와 연결된 개수 
+링크점수:기본점수/외부링크 수 총합
+
+한 웹페이지의 링크점수는 해당 웹페이지로 링크가 걸린 다른 웹페이지의 기본점수 ÷ 외부 링크 수의 총합이다.
+"""
 from collections import defaultdict
 from collections import deque
 
