5910a0857bf14851d370f2ca7be1d92b8121ffb7
minkyeom <minkyeom@naver.com>
2024-09-24 16:57:03 +0900

    algorism_review

--- Programmers/길 찾기 게임.py
+++ Programmers/길 찾기 게임.py
@@ -10,6 +10,135 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42892
 from collections import defaultdict, deque
 import heapq
 
+
+def root_parent(root_num, graph, xy_to_point):
+    for p, son in graph.items():
+        if root_num in son:
+            return xy_to_point[p]
+
+
+def solution(nodeinfo):
+    # 전위 순회
+    before = []
+    # 후위 순회
+    after = []
+
+    # point level
+    level = defaultdict(list)
+
+    # graph
+    graph = defaultdict(list)
+
+    # point search
+    point_number = defaultdict(int)
+    xy_to_point = defaultdict(tuple)
+
+    start = set()
+
+    for num, node in enumerate(nodeinfo):
+        point_number
+        x, y = node[0], node[1]
+        point_number[(x, y)] = num + 1
+        xy_to_point[num + 1] = (x, y)
+
+        # (x,y) and 해당 좌표 번호
+        level[y].append(x)
+        start.add(y)
+
+    for s in start:
+        level[s].sort()
+
+    # 위상에 따라 구분
+    start = list(start)
+    start.sort(reverse=True)
+
+    start = deque(start)
+    visit = set()
+
+    # 진행도
+    progress = 0
+    finish = len(start)
+
+    while start:
+        k = start.popleft()
+        m = level[k]
+
+        if len(visit) == 0:
+            # 현재 위상
+            now_level = k
+            # 해당 좌표 번호
+            root_num = point_number[(m[0], k)]
+            visit.add(k)
+            progress += 1
+            continue
+
+        for i in m:
+            j, num = k, point_number[(i, k)]
+
+            if progress == 1:
+                graph[root_num].append(num)
+
+            else:
+                for node in level[root_now]:
+                    root_x, root_y, root_num = node, root_now, point_number[(node, root_now)]
+
+                    # 맨 왼쪽
+                    if node == 0 and i < root_x:
+                        graph[root_num].append(num)
+                        break
+
+                    # 맨 오른쪽
+                    elif node == len(level[root_now]) - 1 and i > root_x:
+                        graph[root_num].append(num)
+                        break
+
+                    else:
+                        check = root_parent(root_num, graph, xy_to_point)
+                        root_parent_x, root_parent_y = check[0], check[1]
+
+                        # root_parent_x,root_parent_y=root_parent(root_num,graph,xy_to_point)
+                        # print(root_parent_x,root_parent_y,"check")
+
+                        # left에 배치될 경우 확인
+                        if i < root_x and i > root_parent_x:
+                            graph[root_num].append(num)
+                            break
+                        elif i < root_x and i < root_parent_x:
+                            graph[root_num].append(num)
+                            break
+
+                        # right
+                        elif i > root_x and i < root_parent_x:
+                            graph[root_num].append(num)
+
+                    elif i > root_x and i < root_parent_x:
+                    continue
+                    else:
+                    print(i, root_x, root_parent_x)
+                    return 10000
+                    continue
+
+
+# 현재 위상 변경
+root_now = k
+
+progress += 1
+
+if progress == finish:
+    break
+
+print(graph, "last")
+
+return 0
+
+# 내 풀이_ 개선 중
+"""
+전위순회:dfs
+후위순회:자식 노드 없을 경우에 추가
+"""
+from collections import defaultdict, deque
+import heapq
+
 def solution(nodeinfo):
     # 전위 순회
     before = []
