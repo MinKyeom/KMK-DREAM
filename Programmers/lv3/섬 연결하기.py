62f398a446dc0c27e7ae1734dc25576543cac961
minkyeom <minkyeom@naver.com>
2024-10-04 17:11:40 +0900

    algorism_review

--- Programmers/lv3/섬 연결하기.py
+++ Programmers/lv3/섬 연결하기.py
@@ -3,6 +3,71 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/42861
 """
 # 내 풀이 개선 중
+"""
+중간에 빠진 사고 과정:중복된 도로를 제거 안함
+"""
+from collections import defaultdict, deque
+import copy
+
+
+def solution(n, costs):
+    # 비용
+    c = defaultdict(int)
+    # 연결 경로
+    r = defaultdict(set)
+    result = float("inf")
+    island = set([])
+    all_road = set([])
+    for a, b, cost in costs:
+        all_road.add((a, b))
+        all_road.add((b, a))
+        r[a].add(b)
+        r[b].add(a)
+        c[(a, b)] = cost
+        c[(b, a)] = cost
+        island.add(a)
+        island.add(b)
+
+    # 각 섬의 최단 경로 재갱신 모두 비교
+    island = deque(list(island))
+    while island:
+        is_ = island.popleft()
+        q = deque([])
+        visit = [float("inf")] * n
+        # visit[is_]=0
+
+        visit_navi = defaultdict(list)
+
+        for i in r[is_]:
+            q.append((is_, i, 0, []))
+
+        while q:
+            start, end, cost, navi = q.popleft()
+
+            if cost + c[(start, end)] < visit[end]:
+                visit[end] = cost + c[(start, end)]
+                visit_navi[end] = navi + [(start, end), (end, start)]
+
+                for new in r[end]:
+                    q.append((end, new, cost + c[(start, end)], navi + [(start, end), (end, start)]))
+
+        # 최종 경로 비용 산출
+        count = 0
+        check_road = set([])
+
+        for end, navi in visit_navi.items():
+            check_road = check_road | set(navi)
+
+        all_road = all_road & check_road
+        print(all_road)
+
+    for build in all_road:
+        count += c[build]
+
+    # result=min(result,count//2)
+
+    return count
+# 내 풀이 개선 중
 from collections import defaultdict, deque
 import copy
 
