e5847a49176515c3c245a90de9b111a8ab52f4f9
minkyeom <minkyeom@naver.com>
2024-10-05 13:20:56 +0900

    algorism_review

--- Programmers/lv3/섬 연결하기.py
+++ Programmers/lv3/섬 연결하기.py
@@ -15,6 +15,52 @@ def solution(n, costs):
     c = defaultdict(int)
     # 연결 경로
     r = defaultdict(set)
+
+    q = deque([])
+
+    max_cost = 0
+
+    for a, b, cost in costs:
+        r[a].add(b)
+        r[b].add(a)
+        c[(a, b)] = cost
+        c[(b, a)] = cost
+        q.append((b, cost, set([b]), 0))
+        q.append((a, cost, set([a]), 0))
+        max_cost += cost
+
+    result = []
+
+    while q:
+        start, cost, visit, count = q.popleft()
+
+        if max_cost < cost or count > len(costs):
+            continue
+
+        if len(visit) == n:
+            result.append(cost)
+
+        else:
+            for m in r[start]:
+                q.append((m, cost + c[(start, m)], visit | set([m]), count + 1))
+
+    if len(result) > 0:
+        print(min(result))
+
+    return 0
+# 내 풀이 개선 중
+"""
+중간에 빠진 사고 과정:중복된 도로를 제거 안함
+"""
+from collections import defaultdict, deque
+import copy
+
+
+def solution(n, costs):
+    # 비용
+    c = defaultdict(int)
+    # 연결 경로
+    r = defaultdict(set)
     result = float("inf")
     island = set([])
     all_road = set([])
@@ -172,3 +218,22 @@ def solution(n, costs):
         result = min(result, count)
 
     return result
+
+
+# 다른 사람 풀이
+def solution(n, costs):
+    answer = 0
+    costs.sort(key=lambda x: x[2])  # 비용기준으로 오름차순 정렬
+    connect = set([costs[0][0]])  # 연결을 확인하는 집합
+
+    # Kruskal 알고리즘으로 최소 비용 구하기
+    while len(connect) != n:
+        for cost in costs:
+            if cost[0] in connect and cost[1] in connect:
+                continue
+            if cost[0] in connect or cost[1] in connect:
+                connect.update([cost[0], cost[1]])
+                answer += cost[2]
+                break
+
+    return answer
