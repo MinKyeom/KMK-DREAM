044f740aa2f4bb27322d74313810dfea52da8dda
minkyeom <minkyeom@naver.com>
2024-10-29 14:38:49 +0900

    algorism_review

--- Programmers/lv3/기지국 설치.py
+++ Programmers/lv3/기지국 설치.py
@@ -190,3 +190,17 @@ def solution(n, stations, w):
 
     return result + len(check)
 
+# 다른 사람 풀이
+def solution(n, stations, w):
+    ans = 0
+    idx = 0
+    location = 1
+
+    while(location <= n) :
+        if(idx < len(stations) and location >= stations[idx]-w) :
+            location = stations[idx]+w+1
+            idx += 1
+        else :
+            location += 2*w+1
+            ans += 1
+    return ans
