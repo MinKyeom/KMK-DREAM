d5903b483e2c6c4f58b0f353aa2f25d229558060
minkyeom <minkyeom@naver.com>
2024-11-20 16:33:08 +0900

    algorism_review

--- Programmers/lv3/최고의 집합.py
+++ Programmers/lv3/최고의 집합.py
@@ -2,6 +2,73 @@
 출처:프로그래머스
 https://school.programmers.co.kr/learn/courses/30/lessons/12938
 """
+# 내 풀이
+from collections import deque, defaultdict
+
+"""
+def solution(n, s):
+    if s==1:
+        return [-1] 
+
+    result=0
+    q=deque([])
+
+    for i in range(s//n,s):
+        q.append([i])
+
+    while q:
+        k=q.popleft()
+
+        if sum(k)==s and len(k)==n:
+            new=1
+            for t in k:
+                new*=t
+            if new>result:
+                result=new
+                result_set=k
+
+        elif len(k)>=n:
+            continue
+
+        else:
+            if s-sum(k)==1 and len(k)-1==n:
+                new=1
+                for t in k:
+                    new*=t
+                    if new>result:
+                        result=new
+                        result_set=k+[1]
+
+            elif s-sum(k)==2 and len(k)-1==n:
+                k=k+[2]
+                new=1
+                for t in k:
+                    new*=t
+                    if new>result:
+                        result=new
+                        result_set=k
+            else:
+                for num in range(s//n,s-sum(k)+1):
+                    q.append(k+[num])
+
+    return result_set
+    """
+
+
+def solution(n, s):
+    if n > s:
+        return [-1]
+
+    if s % n == 0:
+        return [s // n] * n
+
+    else:
+        k = s % n
+        one = [s // n + 1] * k
+        two = [s // n] * (n - k)
+        result = two + one
+
+    return result
 
 # 내 풀이_개선 중
 from collections import deque
