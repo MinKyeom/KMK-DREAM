4e27fce41d35b312753b341a7aa31604a03e906b
minkyeom <minkyeom@naver.com>
2024-11-21 15:47:45 +0900

    algorism_review

--- Programmers/lv3/최고의 집합.py
+++ Programmers/lv3/최고의 집합.py
@@ -94,3 +94,20 @@ def solution(n, s):
     print(result_set)
 
     return 0
+
+# 다른 사람 풀이
+def bestSet(n, s):
+    answer = []
+    a = int(s/n)
+
+    if a == 0:
+        return [-1]
+
+    b = s%n
+
+    for i in range(n-b):
+        answer.append(a)
+    for i in range(b):
+        answer.append(a+1)
+
+    return answer
