0e9efae433ce27871279447719bfa806051974f3
minkyeom <minkyeom@naver.com>
2024-11-29 16:36:35 +0900

    algorism_review

--- Programmers/lv3/선입 선출 스케줄링.py
+++ Programmers/lv3/선입 선출 스케줄링.py
@@ -89,3 +89,34 @@ def solution(n, cores):
         count += 1
 
     return core_num
+
+# 다른 사람 풀이
+def solution(n, cores):
+    answer = -1
+    time = 50000*10000
+    minP = 0
+    maxP = 50000*10000
+    l = len(cores)
+    rest = n-l
+
+    while minP<=maxP:
+        mid = (minP+maxP)//2
+        completedWork = 0
+        for c in cores:
+            completedWork += mid//c
+
+        if completedWork<(rest):
+            minP = mid+1
+        else:
+            if time>mid:
+                time = mid
+            maxP = mid-1
+
+    for i in cores:
+        rest -= (time-1)//i
+
+    while rest is not 0:
+        answer+=1
+        rest-= 1 if time%cores[answer]==0 else 0
+
+    return answer+1
