111cfc44ea4c525cd3991601bf86a1badb174883
minkyeom <minkyeom@naver.com>
2024-12-02 20:43:04 +0900

    algorism_review

--- Programmers/lv3/거스름돈.py
+++ Programmers/lv3/거스름돈.py
@@ -3,6 +3,48 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/12907
 """
 
+def solution(n, money):
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    for m in money:
+        for now in range(m, n + 1):
+            if now >= m:
+                dp[now] += dp[now - m]
+
+    # print(dp)
+
+    return dp[n] % 1000000007
+
+# 내 풀이_ 개선 중
+"""
+
+from collections import Counter,deque,defaultdict
+import copy
+import heapq
+
+def solution(n, money):
+    q=deque([(n,defaultdict(int))])
+    result=[]
+
+    while q:
+        count,num =q.popleft()
+
+        for i in money:
+            if count > i:
+                new=copy.deepcopy(num)
+                new[i]+=1
+                q.append((count-i,new))
+
+            elif count == i:
+                new=copy.deepcopy(num)
+                new[i]+=1
+                if not new in result:
+                    result.append(new)
+
+
+    return len(result)%1000000007
+    """
+
 # 내 풀이_개선 중
 from collections import Counter, deque
 
