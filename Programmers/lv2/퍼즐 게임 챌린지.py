a216d80c6e901593d2defdb0008b350ddc0ae635
minkyeom <minkyeom@naver.com>
2024-12-06 16:39:07 +0900

    algorism_review

--- Programmers/lv2/퍼즐 게임 챌린지.py
+++ Programmers/lv2/퍼즐 게임 챌린지.py
@@ -3,6 +3,42 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/340212
 """
 
+# 풀이 과정
+# 이분 탐색(최솟값)
+from collections import deque
+
+def solution(diffs, times, limit):
+    before = [0] * len(times)
+    before[0] = times[0]
+
+    for t in range(1, len(times)):
+        before[t] = times[t - 1] + times[t]
+
+    l = 1
+    r = max(diffs) + 1
+    result = float("inf")
+
+    while l <= r:
+        m = (l + r) // 2
+
+        check = list(map(lambda x: max(0, x - m), diffs))
+        t = 0
+
+        for c in range(len(check)):
+            if check[c] == 0:
+                t += times[c]
+            else:
+                t += times[c]
+                t += (before[c]) * (check[c])
+
+        if t <= limit:
+            r = m - 1
+            result = min(result, m)
+        else:
+            l = m + 1
+
+    return result
+
 # 풀이 과정_ 개선 중
 # 이분 탐색(최솟값)
 from collections import deque
