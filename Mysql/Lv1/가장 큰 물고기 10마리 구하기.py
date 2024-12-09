d6039bb09483e532769b94e8793c629afd96fe7c
minkyeom <minkyeom@naver.com>
2024-12-09 16:40:37 +0900

    Mysql Study

---
+++ Mysql/Lv1/가장 큰 물고기 10마리 구하기.py
@@ -0,0 +1,15 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/298517
+"""
+
+# 풀이 과정
+"""
+select ID "ID",LENGTH "LENGTH"
+    
+    from FISH_INFO
+    
+    ORDER BY LENGTH DESC, ID ASC
+    
+    Limit 10;
+"""
