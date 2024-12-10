a13da482edb5ed24ddafaf4c6d87e02faf139ed0
minkyeom <minkyeom@naver.com>
2024-12-10 16:36:41 +0900

    algorism & Mysql

---
+++ Mysql/Lv1/한 해에 잡은 물고기 수 구하기.py
@@ -0,0 +1,12 @@
+"""
+출처: 프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/298516
+"""
+
+# 풀이 과정
+
+"""
+select count(ID) "FISH_COUNT"
+    from FISH_INFO
+    where TIME LIKE "2021%"
+"""
