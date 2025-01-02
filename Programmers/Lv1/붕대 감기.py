676a77b91b0ab5acb7cdbd982bbc2ceaeacde6b4
minkyeom <minkyeom@naver.com>
2025-01-02 16:16:56 +0900

    algorism_review

--- Programmers/Lv1/붕대 감기.py
+++ Programmers/Lv1/붕대 감기.py
@@ -2,7 +2,55 @@
 출처:프로그래머스,
 https://school.programmers.co.kr/learn/courses/30/lessons/250137
 """
+# 풀이 과정
+from collections import deque
 
+
+def solution(bandage, health, attacks):
+    # 초기 체력
+    h = health
+
+    heal = bandage[1]
+    heal_time = bandage[2]
+    sequence_time = bandage[0]
+
+    demege = deque([])
+    demege_time = deque([])
+
+    for i, j in attacks:
+        demege_time.append(i)
+        demege.append(j)
+
+    last = demege_time[-1]
+    time = 0
+    count = 0  # 연속된 시간
+
+    while time < last:
+        if time == demege_time[0] - 1:
+            h -= demege[0]
+            demege_time.popleft()
+            demege.popleft()
+            time += 1
+            # 시간 초기화
+            count = 0
+            continue
+
+        else:
+            if h == health:
+                time += 1
+                count += 1
+                continue
+            else:
+                h += heal
+                h = min(h, health)
+                count += 1
+                time += 1
+                if count % sequence_time == 0:
+                    h += heal_time
+                    h = min(h, health)
+
+        if h <= 0:
+            return -1
 
 # 풀이 과정
 from collections import deque
@@ -56,3 +104,17 @@ def solution(bandage, health, attacks):
         return -1
 
     return h
+
+# 다른 사람 풀이
+def solution(bandage, health, attacks):
+    hp = health
+    start = 1
+    for i, j in attacks:
+        hp += ((i - start) // bandage[0]) * bandage[2] + (i - start) * bandage[1]
+        start = i + 1
+        if hp >= health:
+            hp = health
+        hp -= j
+        if hp <= 0:
+            return -1
+    return hp
