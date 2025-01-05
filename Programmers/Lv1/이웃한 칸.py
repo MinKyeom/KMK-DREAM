35c354f145788f35e3f19f6f511407b2cc9e3a41
minkyeom <minkyeom@naver.com>
2025-01-05 19:50:33 +0900

    algorism_review

--- Programmers/Lv1/이웃한 칸.py
+++ Programmers/Lv1/이웃한 칸.py
@@ -17,3 +17,15 @@ def solution(board, h, w):
                 answer += 1
 
     return answer
+
+# 다른 사람 풀이
+def solution(board, h, w):
+    count = 0
+    n = len(board)
+    dh, dw = [0,1,-1,0],[1,0,0,-1]
+    for i in range(4):
+        h_check, w_check = h+dh[i], w+dw[i]
+        if 0<=h_check<n and 0<=w_check<n:
+            if board[h][w] == board[h_check][w_check]:
+                count += 1
+    return count
