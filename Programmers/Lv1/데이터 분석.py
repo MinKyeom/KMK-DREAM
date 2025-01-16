33b96cb229afa046398244ab890a19debcf80a99
minkyeom <minkyeom@naver.com>
2025-01-16 17:55:01 +0900

    algorism_review

--- Programmers/Lv1/데이터 분석.py
+++ Programmers/Lv1/데이터 분석.py
@@ -41,3 +41,8 @@ def solution(data, ext, val_ext, sort_by):
 def solution(data, ext, val_ext, sort_by):
     column_idx = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
     return sorted(filter(lambda x:x[column_idx[ext]] < val_ext,data),key=lambda x:x[column_idx[sort_by]])
+
+# 다른 사람 풀이
+def solution(data, ext, val_ext, sort_by):
+    column_idx = {"code": 0, "date": 1, "maximum": 2, "remain": 3}
+    return sorted(filter(lambda x:x[column_idx[ext]] < val_ext,data),key=lambda x:x[column_idx[sort_by]])
