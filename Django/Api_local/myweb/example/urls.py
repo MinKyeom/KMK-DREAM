a6d451687183d1551136eddefc0ef54edd782eed
minkyeom <minkyeom@naver.com>
2025-03-30 20:43:38 +0900

    Django_Rest_Framework Study & chatbot base

--- Django/Api_local/myweb/example/urls.py
+++ Django/Api_local/myweb/example/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path,include
-from .views import HelloAPI,bookAPI,booksapi
+from .views import HelloAPI,bookAPI,booksapi,Booksapi,BookAPI,BooksAPIMixins,BookAPIMixins
 
 urlpatterns=[
     path("hello/",HelloAPI),
+    # 함수형 뷰
     path("fbv/books/",booksapi),
     path("fbv/book/<int:bid>/",bookAPI),
+
+    # 클래스형 뷰
+    path("cbv/books/",Booksapi.as_view()),
+    path("cbv/book/<int:bid>/",BookAPI.as_view()),
+
+    #mixin
+    path("mixin/books/",BooksAPIMixins.as_view()),
+    #book_id 랑 views.py에 있는 lookup_field와 일치하게 하기!!!
+    path("mixin/book/<int:book_id>/",BookAPIMixins.as_view()),
 
 ]
--- Django/Api_local/myweb/example/views.py
+++ Django/Api_local/myweb/example/views.py
@@ -33,7 +33,7 @@ class HelloAPI(APIView):
         return Response("hello")
 """
 
-# 책 정보 모델을 받는 api
+# 책 정보 모델을 받는 api 함수뷰
 @api_view(['GET','POST'])
 def booksapi(request):  # /books/ url로 정보를 받았을 때
     if request.method=='GET': # 도서 모델 가져오기
@@ -59,3 +59,56 @@ def bookAPI(request,bid): #url book/bid 요청왔을때
     serializer=BookSerializer(book) # 시리얼라이저 직렬화
 
     return Response(serializer.data , status=status.HTTP_200_OK)
+
+
+
+# 클래스로 받는 api
+class Booksapi(APIView):
+    def get(self,request):
+        books=Book.objects.all()
+        serializer=BookSerializer(books, many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+    def post(self,request):
+        serializer=BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class BookAPI(APIView):
+    def get(self,request,bid):
+        book=get_object_or_404(Book,book_id=bid)
+        serializer=BookSerializer(book)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+#mixin을 활용하는 작업
+from rest_framework import generics
+from rest_framework import mixins
+
+class BooksAPIMixins(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+    queryset=Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request, *args,**kwargs)
+
+class BookAPIMixins(mixins.RetrieveModelMixin,mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin ,generics.GenericAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field ='book_id'
+
+    #추가
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+    #수정
+    def put(self,request,*args,**kwargs):
+        return self.update(request,*args,**kwargs)
+
+    def delete(self,request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
