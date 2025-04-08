aa038a6243d40e200e406e18ed661188615eacb2
minkyeom <minkyeom@naver.com>
2025-04-08 14:51:56 +0900

    Algorism_review

---
+++ Mysql/Lv1/나이 정보가 없는 회원 수 구하기.py
@@ -0,0 +1,13 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/131528
+"""
+
+"""
+SELECT count(USER_ID) "USERS"
+
+from USER_INFO
+
+# wher AGE = NULL or AGE ="NULL"로 출력 안나오는 이유 확인해보기
+where AGE is NULL
+"""
--- Programmers/lv3/정수삼각형.py
+++ Programmers/lv3/정수삼각형.py
@@ -3,7 +3,7 @@
 https://school.programmers.co.kr/learn/courses/30/lessons/43105
 """
 
-#내 풀이
+# 풀이 과정
 import copy
 
 
