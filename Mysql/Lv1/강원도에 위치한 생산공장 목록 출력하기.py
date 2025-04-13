1ed60cf32202acdf038b65e94517b73fc5e64836
minkyeom <minkyeom@naver.com>
2025-04-13 21:47:43 +0900

    Algorism_review

---
+++ Mysql/Lv1/강원도에 위치한 생산공장 목록 출력하기.py
@@ -0,0 +1,14 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/131112
+"""
+
+"""
+SELECT FACTORY_ID,FACTORY_NAME,ADDRESS
+
+from FOOD_FACTORY
+
+where ADDRESS like "강원도%"
+
+order by FACTORY_ID asc
+"""
