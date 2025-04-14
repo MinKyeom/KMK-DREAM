09df7e285b48dcc70df2df5c9cfbf00f78ccfb8b
minkyeom <minkyeom@naver.com>
2025-04-14 21:26:57 +0900

    Mysql

---
+++ Mysql/Lv1/최댓값 구하기.py
@@ -0,0 +1,17 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/59415
+"""
+
+"""
+풀이 과정 
+
+SELECT DATETIME "시간"
+
+from ANIMAL_INS
+
+order by DATETIME desc
+
+limit 1
+
+"""
