beee5377446ba860a13c5bc7ee715ccbb3a3afbb
MinKyeom <minkyeom@naver.com>
2025-04-25 08:27:13 +0900

    Daily_sql_practice

---
+++ Mysql/Lv1/여러 기준으로 정렬하기.py
@@ -0,0 +1,12 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/59404
+"""
+
+"""
+풀이 과정
+
+SELECT ANIMAL_ID,NAME,DATETIME
+from ANIMAL_INS
+order by NAME asc,DATETIME desc
+"""
