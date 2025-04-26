9a3399322b904966d0f79bbb1be14b0f10902161
minkyeom <minkyeom@naver.com>
2025-04-26 19:17:48 +0900

    Algorism_review

--- Programmers/lv2/호텔 대실.py
+++ Programmers/lv2/호텔 대실.py
@@ -1,4 +1,10 @@
-# 내 풀이
+
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/155651
+"""
+
+# 풀이 과정
 def solution(book_time):
     import heapq
     book_time.sort(key=lambda x: (x[0].split(":")))
