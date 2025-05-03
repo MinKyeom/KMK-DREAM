1bef12263377f472977764472f47db8c69e62756
minkyeom <minkyeom@naver.com>
2025-05-03 14:00:33 +0900

    Algorism_review

--- Programmers/lv2/서버 증설 횟수.py
+++ Programmers/lv2/서버 증설 횟수.py
@@ -436,3 +436,17 @@ def solution(players, m, k):
 
     answer = 0
     return answer
+
+# 다른 사람 풀이
+def solution(players, m, k):
+    answer = 0
+    servers = [0]*(24+k)
+    nowmax = m
+    for i,p in enumerate(players):
+        nowmax -= servers[i]*m
+        if nowmax<=p:
+            addservers = ((p-nowmax)//m)+1
+            answer += addservers
+            nowmax+=addservers*m
+            servers[i+k]+=addservers
+    return answer
