43b9ed433e8ec0765f23ac36ffdaed5857beab73
minkyeom <minkyeom@naver.com>
2025-05-07 20:55:39 +0900

    Mysql

---
+++ Mysql/Lv1/아픈 동물 찾기.py
@@ -0,0 +1,12 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/59036
+"""
+
+"""
+풀이 과정
+SELECT ANIMAL_ID,NAME
+from ANIMAL_INS
+where INTAKE_CONDITION="Sick"
+order by ANIMAL_ID
+"""
---
+++ Mysql/Lv1/어린 동물 찾기.py
@@ -0,0 +1,12 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/59037
+"""
+
+"""
+풀이 과정
+SELECT ANIMAL_ID,NAME
+from ANIMAL_INS
+where INTAKE_CONDITION !="Aged"
+order by ANIMAL_ID
+"""
