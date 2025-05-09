1afee776c999bf1037f032e14cf7d8f3410f635c
MinKyeom <minkyeom@naver.com>
2025-05-09 21:25:17 +0900

    Mysql

--- Mysql/Lv2/부모의 형질을 모두 가지는 대장균 찾기.py
+++ Mysql/Lv2/부모의 형질을 모두 가지는 대장균 찾기.py
@@ -2,6 +2,17 @@
   출처:프로그래머스,
   https://school.programmers.co.kr/learn/courses/30/lessons/301647
   """
+  #풀이 과정_개선 중
+  """
+  -- bin(): 이진법 변환 -- 
+  select ID,GENOTYPE,PARENT_ID "PARENT_GENOTYPE"
+  from ECOLI_DATA
+  -- 한 쪽은 현재 내 컬럼 Gentype = 부모 id의 Gentype --
+  -- parent_id의 gentype을 들고 오는 방법 --
+  where bin(GENOTYPE)
+    =
+    (select
+  """
   
   # 풀이 과정_개선 중
   """
