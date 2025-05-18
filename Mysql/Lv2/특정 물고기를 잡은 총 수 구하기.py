f26214037e38312a23cfc2238a9099a3570d31fe
MinKyeom <minkyeom@naver.com>
2025-05-19 08:30:21 +0900

    Mysql

---
+++ Mysql/Lv2/특정 물고기를 잡은 총 수 구하기.py
@@ -0,0 +1,14 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/298518
+"""
+
+# 풀이 과정
+"""
+select count(*) "FISH_COUNT"
+from FISH_INFO as A
+    join FISH_NAME_INFO as B
+    on A.FISH_TYPE = B.FISH_TYPE
+where B.FISH_NAME in ('BASS','SNAPPER')
+    # and LENGTH is not null
+"""
