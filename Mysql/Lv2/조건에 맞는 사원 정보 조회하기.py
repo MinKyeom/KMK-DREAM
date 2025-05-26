0c23e10e881c14a3081f3ec36af7de7fb3c2c063
MinKyeom <minkyeom@naver.com>
2025-05-26 22:30:40 +0900

    Mysql

--- Mysql/Lv2/조건에 맞는 사원 정보 조회하기.py
+++ Mysql/Lv2/조건에 맞는 사원 정보 조회하기.py
@@ -2,6 +2,17 @@
  출처:프로그래머스,
  https://school.programmers.co.kr/learn/courses/30/lessons/284527
 """
+# 풀이과정
+"""
+select C.SCORE "SCORE", C.EMP_NO, B.EMP_NAME, B.POSITION, B.EMAIL
+from HR_DEPARTMENT as A,HR_EMPLOYEES as B,
+( select EMP_NO,YEAR ,sum(score) "SCORE"  
+from HR_GRADE
+group by EMP_NO,YEAR ) as C
+where B.EMP_NO = C.EMP_NO
+order by SCORE desc
+limit 1
+"""
 
 # 풀이과정_개선 중
 """
