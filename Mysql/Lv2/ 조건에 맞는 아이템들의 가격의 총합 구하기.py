59a0c0ec565e63d6cb197823f362fb8d69417acd
MinKyeom <minkyeom@naver.com>
2025-05-30 08:17:17 +0900

    Mysql

---
+++ Mysql/Lv2/ 조건에 맞는 아이템들의 가격의 총합 구하기.py
@@ -0,0 +1,13 @@
+"""
+출처:프로그래머스,
+https://school.programmers.co.kr/learn/courses/30/lessons/273709
+"""
+
+#풀이 과정
+"""
+
+select sum(PRICE) as TOTAL_PRICE
+from ITEM_INFO
+where RARITY = 'LEGEND'
+
+"""
