0d58e2f9687acaf5ff5b15b2d5ab45a0ec30e61d
MinKyeom <minkyeom@naver.com>
2025-05-31 18:40:56 +0900

    Java Start

---
+++ Java/Basic/start.java
@@ -0,0 +1,11 @@
+// public class start {
+//   public static void start(String[] args) { 
+//     System.out.println("Hello, Java start");
+//   }  
+// }
+
+public class start {
+  public static void main(String[] args) { 
+    System.out.println("Hello, Java start");
+  }  
+}
