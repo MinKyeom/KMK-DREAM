6cada0755adf8f233c2acf084b0fcc0924d36321
MinKyeom <minkyeom@naver.com>
2025-06-06 14:56:26 +0900

    Spring_boot_start

---
+++ Spring_boot/helloController.java
@@ -0,0 +1,23 @@
+// package Spring_boot_project.blog.src.test.java.com.mkspring.blog;
+// import org.springframework.streotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+// public class helloController {
+//   @GetMapping("/hello");
+// }
+// package com.mkspring.blog;
+
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
+
+// @Controller
+// public class HelloController {
+
+//     @GetMapping("/hello")
+//     @ResponseBody
+//     public String hello() {
+//         return "Hello from Spring Boot!";
+//     }
+// }
