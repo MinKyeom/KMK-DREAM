98d2be412664ba0116409894625ff8d9900ece59
MinKyeom <minkyeom@naver.com>
2025-06-19 08:29:35 +0900

    Spring Boot

--- Spring_boot/demo/question/QuestionController.java
+++ Spring_boot/demo/question/QuestionController.java
@@ -7,9 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.ResponseBody; 
 import org.springframework.web.bind.annotation.PathVariable; 
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController { 
@@ -17,7 +19,7 @@ public class QuestionController {
   // private final QuestionRepository questionRepository;
   private final QuestionService questionService;
 
-  @GetMapping("/question/list")
+  @GetMapping("/list")
   // @ResponseBody
   public String list(Model model){
     List<Question> questionList = this.questionService.getList();
@@ -25,7 +27,7 @@ public class QuestionController {
     return "question_list";
   }
 
-  @GetMapping(value = "/question/detail/{id}")
+  @GetMapping(value = "/detail/{id}")
   public String detail(Model model,@PathVariable("id") Integer id){
     Question question = this.questionService.getQuestion(id);
     model.addAttribute("question",question);
