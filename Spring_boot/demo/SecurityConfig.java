d313a946c9b743707d4cec2ae81c413b654ed60a
MinKyeom <minkyeom@naver.com>
2025-06-28 15:25:30 +0900

    Spring Boot

---
+++ Spring_boot/demo/SecurityConfig.java
@@ -0,0 +1,75 @@
+// package com.example.demo;
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.security.web.util.matcher.AndRequestMatcher;
+// import org.springframework.util.AntPathMatcher;
+
+// @Configuration
+// @EnableWebSecurity
+// public class SecurityConfig {
+//   @Bean
+//   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//     http 
+//         .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+//           .requestMatchers(new AntPathMatcher("/**")).permitAll() )
+          
+//         .csrf((csrf) -> csrf
+//         .ignoringRequestMatchers(new AntPathMatcher("/h2-console/**")));
+        
+//         return http.build();
+//   }
+  
+// }
+
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http 
+        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+          .requestMatchers(new AntPathRequestMatcher("/**")).permitAll() ) // 올바르게 AntPathRequestMatcher 사용
+
+        .csrf((csrf) -> csrf
+        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))) // 올바르게 AntPathRequestMatcher 사용
+        
+//         .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+//           .requestMatchers(new AntPathMatcher("/**")).permitAll() )
+          
+//         .csrf((csrf) -> csrf
+//         .ignoringRequestMatchers(new AntPathMatcher("/h2-console/**")));
+        .headers((headers) -> headers
+          .addHeaderWriter(new XFrameOptionsHeaderWriter(
+            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+          
+        .formLogin( (formLogin) -> formLogin
+          .loginPage("/user/login")
+          .defaultSuccessUrl("/") 
+          );
+    return http.build();
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+  
+}
--- Spring_boot/demo/user/UserService.java
+++ Spring_boot/demo/user/UserService.java
@@ -1,5 +1,6 @@
 package com.example.demo.user;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +13,14 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
   public SiteUser create(String username, String email, String password) {
 
     SiteUser user = new SiteUser();
     user.setUsername(username);
     user.setEmail(email);
-    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    // BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     user.setPassword(passwordEncoder.encode(password));
     this.userRepository.save(user);
     return user;
