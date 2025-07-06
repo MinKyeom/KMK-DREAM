21f16d8556188f3fb85dfe88fed69d953e936cb2
MinKyeom <minkyeom@naver.com>
2025-07-06 17:54:55 +0900

    Algorism_review

--- Programmers/lv2/완전범죄.py
+++ Programmers/lv2/완전범죄.py
@@ -308,3 +308,38 @@ def solution(info, n, m):
                 continue
     
     return -1 if len(result) = 0 else min(result)
+
+# 다른 사람 풀이
+def solution(info, n, m):
+    MAX = 120 
+
+    dp = [[False] * MAX for _ in range(MAX)]
+    dp[0][0] = True 
+
+    item_count = len(info)
+
+ 
+    for traceA, traceB in info:
+        next_dp = [[False] * MAX for _ in range(MAX)]
+
+        for a in range(n):  
+            for b in range(m): 
+                if not dp[a][b]:
+                    continue
+
+               
+                if a + traceA < n:
+                    next_dp[a + traceA][b] = True
+
+                
+                if b + traceB < m:
+                    next_dp[a][b + traceB] = True
+
+        dp = [row[:] for row in next_dp]
+
+    for a in range(n):
+        for b in range(m):
+            if dp[a][b]: 
+                return a
+
+    return -1 
