6bca4dc4f6cc6e86c0b219d54881cb416cc37223
MinKyeom <minkyeom@naver.com>
2025-07-10 08:37:04 +0900

    Spring Boot Add markdown function

--- Spring_boot/demo/answer/AnswerController.java
+++ Spring_boot/demo/answer/AnswerController.java
@@ -44,11 +44,12 @@ public class AnswerController {
       model.addAttribute("question", question);
       return "question_detail";
     }
-  this.answerService.create(question, answerForm.getContent(),siteUser);
+
+  Answer answer =this.answerService.create(question, answerForm.getContent(),siteUser);
   // @RequestParam(value="content") String content) {
   //   Question question = this.questionService.getQuestion(id);
   //   this.answerService.create(question,content);
-    return String.format("redirect:/question/detail/%s",id);
+    return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(),answer.getId());
   }
 
   // public SomeData getMethodName(@RequestParam String param) {
@@ -83,7 +84,7 @@ public class AnswerController {
     }
 
     this.answerService.modify(answer, answerForm.getContent());
-    return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(),answer.getId());
 
   }
 
@@ -105,7 +106,10 @@ public class AnswerController {
     Answer answer = this.answerService.getAnswer(id);
     SiteUser siteUser = this.userService.getUser(principal.getName());
     this.answerService.vote(answer,siteUser);
-    return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+    // return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+
+    return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(),answer.getId());
+
   }
 }
 
