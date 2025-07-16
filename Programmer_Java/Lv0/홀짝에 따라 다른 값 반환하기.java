5b36b92156363d0a74b01fbf525660a76b94c4f7
MinKyeom <minkyeom@naver.com>
2025-07-17 08:00:54 +0900

    Basic Java Algorism

---
+++ Programmer_Java/Lv0/홀짝에 따라 다른 값 반환하기.java
@@ -0,0 +1,29 @@
+// package Java_Programmers.Lv0;
+
+// public class 홀짝에 따라 다른 값 반환하기 {
+  
+// }
+
+
+/*
+//출처: 프로그래머스,
+//https://school.programmers.co.kr/learn/courses/30/lessons/181935
+class Solution {
+    public int solution(int n) {
+        int result = 0;
+        if(n%2==1){
+            for(int i=1; i<=n; i+=2){
+                result+=i;
+            }
+                
+        }
+        else{
+            for (int i=2; i<=n; i+=2){
+                result+=(int)Math.pow(i,2);
+            }
+        }
+        
+        return result;
+    }
+}
+*/
