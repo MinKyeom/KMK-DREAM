4d182dd22d8c9f1ef738583b50a34f54647387d5
MinKyeom <minkyeom@naver.com>
2025-07-19 18:08:08 +0900

    Java Basic Study

---
+++ Java/Basic/Hello.java
@@ -0,0 +1,24 @@
+package src.Java_Basic;
+
+public class Hello {
+  
+  // public  < 클래스 선언부
+  // Hello < 클래스 이름
+  // public class < 공개 클래스
+
+  // 클래스 이름은 소스 파일명과 동일해야 하며, 대소문자도 일치해야 한다.
+
+  // public static void main(String[] args) 메소드 선언부 
+  // 여기서 main 메소드 이름 < main 메소드가 프로그램 실행의 진입점
+
+  // 자바는 하나의 변수는 다양한 값 저장 불가능 
+  // ex) int age; < int를 저장할 수 있는 age 변수 선언 
+  //    double value; 실수 double를 저장 할 수 있는 value 변수 선언
+
+  // 자바에서는 int score; < 변수 선언, score = 90; < 변수 저장 ! 
+  // 저장을 안하면 변수 생성 X 
+  // int score = 90; < 선언과 동시에 저장하는 방법
+  //맞아요, **자바(Java)**는 변수를 선언하지 않으면 값을 저장하거나 사용X
+  //자바는 **정적 타입 언어(static typed language)**이기 때문에, 모든 변수는 반드시 자료형과 함께 선언되어야 합니다.
+
+}
