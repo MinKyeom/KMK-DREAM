e20ef3aab15c84ef2f1357ed9d40b356315c5c3d
MinKyeom <minkyeom@naver.com>
2025-07-20 19:28:13 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/조건 문자열.java
@@ -0,0 +1,52 @@
+// package Programmers;
+
+// public class 조건 문자열 {
+  
+// }
+
+// 출처: 프로그래머스
+//https://school.programmers.co.kr/learn/courses/30/lessons/181934
+
+// 풀이 과정 개선 중 
+/*
+ * // eq,ineq 조합 한 후 <,= 구현을 if문으로 구분하고나서 구하기
+
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        if(eq=="=" && ineq == ">"){
+            if ( n >= m){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        elseif(eq=="=" && ineq=="<"){
+            if (n<=m){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        elseif (eq=="!" && ineq==">"){
+            if (n>m){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        else {
+            if(n<m){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        }
+        }
+            }
+        }
+    }
+}
+ * 
+ */
