b1e58dd4389228e2271eb9ee69b4bee4c0f82a00
MinKyeom <minkyeom@naver.com>
2025-07-23 08:19:10 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/등차수열의 특별만 항만 더하기.java
@@ -0,0 +1,27 @@
+// package Programmers;
+
+// public class 등차수열의 특별만 항만 더하기 {
+  
+// }
+//출처: 프로그래머스,
+//https://school.programmers.co.kr/learn/courses/30/lessons/181931
+
+// 풀이 과정
+
+/*
+ * class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        int check = included.length;
+        
+        for(int i =0; i<check; i++){
+            if(included[i]){
+                answer+=(a+d*(i));
+            }
+            
+        }
+        
+        return answer;
+    }
+}
+ */
--- Programmer_Java/Lv0/코드 처리하기.java
+++ Programmer_Java/Lv0/코드 처리하기.java
@@ -6,6 +6,48 @@
 
 // 출처: 프로그래머스
 // https://school.programmers.co.kr/learn/courses/30/lessons/181932
+// 풀이 과정
+/*
+ * class Solution {
+    public String solution(String code) {
+        // 시작 할 떄 모드 
+        int mode = 0;
+        int num = code.length();
+        String result = "";
+        
+        for(int i=0; i < num; i++){
+            if(code.charAt(i)=='1'){
+                if(mode==1){
+                    mode = 0;
+                }
+                else{
+                    mode = 1; 
+                }
+            }
+            
+            else{
+                if(mode==0){
+                    if(i%2==0){
+                        result+=code.charAt(i);
+                    }
+                }
+                else{
+                    if(i%2==1){
+                        result+=code.charAt(i);
+                    }
+                }
+            }
+        }
+        if(result.length()>0){
+            return result;
+        }
+        else{
+            return "EMPTY";
+        }
+    }
+}
+ */
+
 
 // 풀이과정_ 개선 중 
 /*
