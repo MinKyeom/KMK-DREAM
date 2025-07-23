d569a16c93ee8bee9ca79b1a2cb03a73c9e2a521
MinKyeom <minkyeom@naver.com>
2025-07-24 08:24:46 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/주사위 게임2.java
@@ -0,0 +1,35 @@
+// package Programmers;
+
+// public class 주사위 게임2 {
+  
+// }
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181930
+
+/* 
+class Solution {
+    public int solution(int a, int b, int c) {
+        if(a==b && a!=c){
+            double result =(a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+            return (int) result;
+        }
+        else if (a==c && a!=b){
+            double result =(a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+            return (int) result;
+        }
+        
+        else if (b==c && b!=a){
+            double result =(a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2));
+            return (int) result;
+        }
+        else if (b==c && a==c){
+            double result =(a+b+c)*(Math.pow(a,2)+Math.pow(b,2)+Math.pow(c,2))*(Math.pow(a,3)+Math.pow(b,3)+Math.pow(c,3));
+            return (int) result;
+        }
+        else{
+            return a+b+c; 
+        }
+        
+    }
+}
+*/
