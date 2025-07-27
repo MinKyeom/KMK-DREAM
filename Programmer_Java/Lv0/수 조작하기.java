bf6bfaaedbd0066c1f77f7020a58557cdb9cde0c
MinKyeom <minkyeom@naver.com>
2025-07-28 08:07:57 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/수 조작하기.java
@@ -0,0 +1,32 @@
+// package Programmers;
+
+// public class 수 조작하기 {
+  
+// }
+
+//출처:프로그래머스
+//https://school.programmers.co.kr/learn/courses/30/lessons/181926
+
+/* 
+class Solution {
+    public int solution(int n, String control) {
+        char[] check = n.toCharArray();
+        for(int i=0; i<check.length; i++){
+            if(check[i]=='w'){
+                n+=1;
+            }
+            else if(check[i]=='s'){
+                n-=1;
+            }
+            else if(check[i]=='d'){
+                n+=10;
+            }
+            else if(checl[i]=='a'){
+                n-=10;
+            }
+        }
+        
+        return n;
+    }
+}
+*/
