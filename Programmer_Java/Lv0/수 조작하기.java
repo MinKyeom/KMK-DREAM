347ba4a6d3c0eb5cc0eb112630753545c4f871d7
MinKyeom <minkyeom@naver.com>
2025-07-30 08:17:35 +0900

    Java Basic Study

--- Programmer_Java/Lv0/수 조작하기.java
+++ Programmer_Java/Lv0/수 조작하기.java
@@ -6,7 +6,32 @@
 
 //출처:프로그래머스
 //https://school.programmers.co.kr/learn/courses/30/lessons/181926
+// 풀이과정
+/* 
+class Solution {
+    public int solution(int n, String control) {
+        char[] check = control.toCharArray();
+        for(int i=0; i<check.length; i++){
+            if(check[i]=='w'){
+                n+=1;
+            }
+            else if(check[i]=='s'){
+                n-=1;
+            }
+            else if(check[i]=='d'){
+                n+=10;
+            }
+            else if(check[i]=='a'){
+                n-=10;
+            }
+        }
+        
+        return n;
+    }
+}
+*/
 
+// 풀이과정 개선중
 /* 
 class Solution {
     public int solution(int n, String control) {
---
+++ Programmer_Java/Lv0/수열과 구간쿼리3.java
@@ -0,0 +1,25 @@
+// package Programmers;
+
+// public class 수열과 구간쿼리3 {
+  
+// }
+
+//출처:프로그래머스,
+//https://school.programmers.co.kr/learn/courses/30/lessons/181924?language=java
+
+/*
+ * class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for(int[] change: queries){
+            int i = change[0];
+            int j = change[1];
+            int fir = arr[i];
+            int sec = arr[j];
+            arr[i] = sec;
+            arr[j] = fir;
+        }
+        
+        return arr;
+    }
+}
+ */
