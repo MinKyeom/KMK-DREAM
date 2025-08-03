9e94a9ff8c3ef7e4c1d31116514f2e107428f9e2
MinKyeom <minkyeom@naver.com>
2025-08-03 15:58:46 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/카운트 업.java
@@ -0,0 +1,21 @@
+// package Programmers;
+
+// public class 카운트 업 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181920
+
+/*
+ * class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] result = new int[end_num-start_num+1];
+        for(int i=0; i<=end_num-start_num; i++ ){
+            result[i] =i+start_num;
+        }
+
+        return result;
+    }
+}
+ */
