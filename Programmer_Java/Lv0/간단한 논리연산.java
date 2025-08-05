40f01f6283a682271a0d6f48b70ee2107b449329
MinKyeom <minkyeom@naver.com>
2025-08-06 08:01:47 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/간단한 논리연산.java
@@ -0,0 +1,41 @@
+// package Programmers;
+
+// public class 간단한 논리연산 {
+  
+// }
+
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181917
+
+/*
+ * class Solution {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean answer = true;
+        boolean first;
+        boolean second;
+        
+        if(x1||x2){
+            first = true;
+        }
+        else{
+            first = false; 
+        }
+        
+        if(x3||x4){
+            second = true;
+        }
+        else{
+            second = false;
+        }
+        
+        if(first&&second){
+            answer = true;
+        }
+        else{
+            answer = false;
+        }
+        return answer;
+    }
+}
+ */
