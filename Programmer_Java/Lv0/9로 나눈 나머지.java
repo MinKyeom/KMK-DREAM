9104ccccb8aefa026b65b22b2d02699736334bf5
MinKyeom <minkyeom@naver.com>
2025-08-07 08:13:19 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/9로 나눈 나머지.java
@@ -0,0 +1,27 @@
+// package Programmers;
+
+// public class 9로 나눈 나머지 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181914
+
+/*
+ * class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        String[] num_split=number.split("");
+        int num;
+        
+        for(String k:num_split){
+            num = Integer.parseInt(k);
+            answer+=num;
+        }
+        
+        answer = answer%9;
+        
+        return answer;
+    }
+}
+ */
---
+++ Programmer_Java/Lv0/글자 이어 붙여 문자열 만들기.java
@@ -0,0 +1,21 @@
+// package Programmers;
+
+// public class 글자 이어 붙여 문자열 만들기 {
+  
+// }
+
+//출처: 프로그래머스,
+//https://school.programmers.co.kr/learn/courses/30/lessons/181915
+
+/*
+ * class Solution {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+        String[] words = my_string.split("");
+        for(int i:index_list){
+            answer+=words[i];
+        }
+        return answer;
+    }
+}
+ */
