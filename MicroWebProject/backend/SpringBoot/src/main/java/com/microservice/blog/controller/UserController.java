282557d532184da74beee508bb48fddaf86f2ba1
MinKyeom <minkyeom@naver.com>
2025-08-16 14:02:33 +0900

    Mirco Web Service Project

--- MicroWebProject/backend/SpringBoot/src/main/java/com/microservice/blog/controller/UserController.java
+++ MicroWebProject/backend/SpringBoot/src/main/java/com/microservice/blog/controller/UserController.java
@@ -1,3 +1,6 @@
+/*
+ * 이해과정: 자바 스프링에서는 모든 파일은 패키지가 필요하다 default로 지정해버리면 파일 이름이 같으면 구분불가
+ */
 package com.microservice.blog.controller;
 
 
@@ -7,7 +10,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+/*
+ * 
+이 클래스가 HTTP 요청을 처리하는 컨트롤러라는 것을 나타냄.
+
+메서드에서 리턴하는 값이 **HTTP 응답 본문(response body)**에 직접 담기도록 해줌 (JSON, XML 등).
+
+HTML 뷰를 반환하지 않고, 데이터(JSON 등)만 반환.
+ */
 @RestController
+
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
