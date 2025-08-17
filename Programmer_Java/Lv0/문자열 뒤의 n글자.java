ac4330df871f1f495a8c879674986b3e8f6c0f49
MinKyeom <minkyeom@naver.com>
2025-08-18 08:11:21 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/문자열 뒤의 n글자.java
@@ -0,0 +1,26 @@
+// package Programmers;
+
+// public class 문자열 뒤의 n글자 {
+  
+// }
+
+
+// 출처:프로그래머스.
+// https://school.programmers.co.kr/learn/courses/30/lessons/181910
+
+//풀이 과정
+
+/*
+ * class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] string_split = my_string.split("");
+        
+        for(int i = my_string.length()-n; i<my_string.length(); i++){
+            answer+=string_split[i];
+            
+        }
+        return answer;
+    }
+}
+ */
