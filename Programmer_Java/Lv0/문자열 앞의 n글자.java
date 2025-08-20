2703fa9f2ffe6b55d917153bbfc3a149cf65ee8c
MinKyeom <minkyeom@naver.com>
2025-08-21 07:59:25 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/문자열 앞의 n글자.java
@@ -0,0 +1,22 @@
+// package Programmers;
+
+// public class 문자열 앞의 n글자 {
+  
+// }
+
+// 출처:프로그래머스
+// 
+
+// 풀이과정
+/*
+ * class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] split_string = my_string.split("");
+        for(int i = 0; i<n; i++){
+            answer+=split_string[i];
+        }
+        return answer;
+    }
+}
+ */
