2cd9009fd758a5a274cd16dd89dc52e22c4dc21b
MinKyeom <minkyeom@naver.com>
2025-08-22 08:09:38 +0900

    Micro Web Project

--- MicroWebProject/backend/SpringBoot/src/main/java/com/microservice/blog/dto/UserSignupRequest.java
+++ MicroWebProject/backend/SpringBoot/src/main/java/com/microservice/blog/dto/UserSignupRequest.java
@@ -1,13 +1,40 @@
 package com.microservice.blog.dto;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
-public class UserSignupRequest {
-    private String username;
-    private String email;
-    private String password;
+@Getter
+public class UserSignupRequest{
+  private String email;
+  private String password;
+  private String name;
 }
+
+
+/*
+ *package com.example.demo.dto;
+
+import lombok.Getter;
+
+// 클라이언트가 보내는 회원가입 요청 데이터를 담는 클래스
+@Getter // 각 필드에 대해 getter 자동 생성
+public class UserRequestDto {
+    private String email;    // 이메일
+    private String password; // 비밀번호
+    private String name;     // 이름
+} 
+ * 
+ */
+
+// package com.microservice.blog.dto;
+
+// import lombok.Data;
+
+// @Data
+// public class UserSignupRequest {
+//     private String username;
+//     private String email;
+//     private String password;
+// }
 
 /*
 
