efcc82286f63f0bf91887f0f5273608e80629194
MinKyeom <minkyeom@naver.com>
2025-08-23 16:14:52 +0900

    Java Basic Study

---
+++ Java/Basic/KeyCodeExample.java
@@ -0,0 +1,19 @@
+package src.Java_Basic;
+
+public class KeyCodeExample {
+  public static void main(String[] args) throws Exception{
+    int keyCode;
+
+    keyCode = System.in.read();
+    System.out.println(keyCode);
+
+    keyCode = System.in.read();
+    System.out.println(keyCode);
+
+    keyCode = System.in.read();
+    System.out.println(keyCode);
+
+
+  }
+  
+}
---
+++ Java/Basic/printf.java
@@ -0,0 +1,7 @@
+package src.Java_Basic;
+
+public class printf {
+  public static void main(String[] args){
+    System.out.printf("이름: %s","이름이뭘까?");
+  }  
+}
