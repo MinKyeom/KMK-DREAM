28d2950022e428a625ccb070e2dfb8141ef7588b
MinKyeom <minkyeom@naver.com>
2025-08-26 08:04:07 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/세로 읽기.java
@@ -0,0 +1,27 @@
+// package Programmers;
+
+// public class 세로 읽기 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181904
+
+
+// 풀이 과정 
+
+/*
+ * class Solution {
+    public String solution(String my_string, int m, int c) {
+        String answer = "";
+        String[] split_string = my_string.split("");
+        for(int i = 0 ; i < split_string.length; i++){
+            if(i%m==c-1){
+                answer+=split_string[i];
+            }
+            
+        }
+        return answer;
+    }
+}
+ */
