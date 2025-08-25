1bd593afc2ca2f465fdbdd319c313c7527a0c37f
MinKyeom <minkyeom@naver.com>
2025-08-25 21:41:24 +0900

    Java Basic Study

---
+++ Java/Basic/ScannerExample.java
@@ -0,0 +1,12 @@
+package src.Java_Basic;
+
+import java.util.*;
+
+public class ScannerExample {
+  public static void main(String[] args) throws Exception{
+    Scanner scanner = new Scanner(System.in);
+
+    String inputData = scanner.nextLine();
+    System.out.println("결과"+inputData);
+  }
+}
