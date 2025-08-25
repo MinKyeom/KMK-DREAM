b7b4183fdd2dbc243ee96b798f76f59df62da119
MinKyeom <minkyeom@naver.com>
2025-08-26 08:43:16 +0900

    Java Basic Study

---
+++ Java/Basic/CompareOperatorExample2.java
@@ -0,0 +1,10 @@
+package src.Java_Basic;
+
+public class CompareOperatorExample2 {
+  public static void main(String[] args){
+    double v4 = 0.1;
+    float v5 = 0.1f;
+    System.out.println(v4==v5);
+    System.out.println((float)v4 == v5);
+  }
+}
