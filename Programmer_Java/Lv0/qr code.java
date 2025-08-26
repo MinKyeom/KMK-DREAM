74fff37c583475bbbca0790f927bd06959a98574
MinKyeom <minkyeom@naver.com>
2025-08-27 08:04:33 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/qr code.java
@@ -0,0 +1,27 @@
+// package Programmers;
+
+// public class qr code {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181903
+
+// 풀이과정 
+/*
+ * class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        String[] split_code = code.split("");
+        
+        for(int i= 0; i<split_code.length; i++){
+            if(i%q == r){
+                answer+=split_code[i];
+            }
+        }
+        
+        return answer;
+        
+    }
+}
+ */
