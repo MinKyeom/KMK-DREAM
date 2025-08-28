a9a9ba3106f12b34e7a37813a8382e0037ed7a5d
MinKyeom <minkyeom@naver.com>
2025-08-29 08:17:45 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/배열 만들기1.java
@@ -0,0 +1,28 @@
+// package Programmers;
+
+// public class 배열 만들기1 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181901
+
+// 풀이과정
+/*
+ * class Solution {
+    public int[] solution(int n, int k) {
+        int[] answer = new int[(int) n/k];
+        int start=0;
+        for(int i =1; i<=n; i++ ){
+            if(i%k==0){
+                answer[start] = i;
+                start++;
+            }
+        }
+        
+        return answer;
+    
+    }
+
+}
+ */
