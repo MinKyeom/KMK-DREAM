893346b4c6ce6c475730b064e5d40070a716af85
MinKyeom <minkyeom@naver.com>
2025-09-01 08:00:30 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/가까운 1찾기.java
@@ -0,0 +1,25 @@
+// package Programmers;
+
+// public class 가까운 1찾기 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181898
+
+// 풀이과정
+/*
+ * class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer;
+        
+        for(int k = idx; k< arr.length; k++){
+            if (arr[k] == 1){
+                return k;
+            }
+        }
+        
+        return -1;
+    }
+}
+ */
