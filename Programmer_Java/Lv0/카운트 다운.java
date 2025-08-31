886a194396387905f59caec19a399f70189e43bd
MinKyeom <minkyeom@naver.com>
2025-08-31 15:06:54 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/카운트 다운.java
@@ -0,0 +1,25 @@
+// package Programmers;
+
+// public class 카운트 다운 {
+  
+// }
+
+
+// 출처: 프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181899
+
+// 풀이 과정 
+/*
+ * class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[start_num-end_num+1];
+        int num = 0;
+        for(int k = start_num; k>=end_num; k--){
+            answer[num] = k;
+            num++;
+        }
+        
+        return answer;
+    }
+}
+ */
