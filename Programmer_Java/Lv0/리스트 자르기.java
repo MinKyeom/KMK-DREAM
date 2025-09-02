a40f5ba3ac31627b3cd08631e9a01124c78b7e61
MinKyeom <minkyeom@naver.com>
2025-09-03 08:27:47 +0900

    Java Basic Study

--- Programmer_Java/Lv0/리스트 자르기.java
+++ Programmer_Java/Lv0/리스트 자르기.java
@@ -7,6 +7,53 @@
 // 출처:프로그래머스,
 // https://school.programmers.co.kr/learn/courses/30/lessons/181897
 
+// 풀이 과정_개선 중 
+/*
+ * class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        
+        if(n==1){
+            int[] answer = new int[b];
+            for(int k =0; k<=b; k++){
+                answer[k] = num_list[k];
+            }
+            return answer;
+        }
+        
+        else if(n==2){
+            int[] answer = new int[num_list.length-a+1];
+            for(int k =a; k<num_list.length; k++){
+                answer[k-a] = num_list[k];
+            }
+            return answer;
+        }
+        
+        else if(n==3){
+            int[] answer = new int[b-a+1];
+                for(int k =a; k<=b; k++){
+                    answer[k-a] = num_list[k];
+                }   
+            return answer;
+            }
+        
+        else{
+            int index_num = (int) (b-a+1)/c +1;
+
+            int[] answer = new int[index_num];
+            int count = 0;    
+                for(int k=a; k<=b; k+=c){
+                    answer[count] = num_list[k];
+                    count++;
+                }
+            return answer;
+            }
+    }
+}
+      
+ */
 
 // 풀이과정_개선 중
 
