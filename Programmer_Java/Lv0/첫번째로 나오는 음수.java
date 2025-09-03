d37cfeb03eb31fef15eedc33ab996c7700f6b5a3
MinKyeom <minkyeom@naver.com>
2025-09-04 07:57:28 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/첫번째로 나오는 음수.java
@@ -0,0 +1,22 @@
+// package Programmers;
+
+// public class 첫번째로 나오는 음수 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181896
+
+// 풀이과정 
+/*
+ * class Solution {
+    public int solution(int[] num_list) {
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]<0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+ */
