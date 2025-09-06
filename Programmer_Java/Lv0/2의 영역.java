bf5ce282e138cf6a8f6e5875ab1fc5d664ae3ee8
MinKyeom <minkyeom@naver.com>
2025-09-06 14:56:33 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/2의 영역.java
@@ -0,0 +1,29 @@
+// package Programmers;
+
+// public class 2의 영역 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181894
+
+/*
+ * // 2 들어가 있는 리스트를 만든 후 
+// 2가 나오긴 전까지 들어갈 리스트를 만든 후 2가 나오면 최종적으로 합쳐주는 방식
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        List<Integer> before = new ArrayList<>();
+        List<Integer> after = new ArrayList<>();
+        for(int i:arr){
+            
+        }
+        return answer;
+        
+    }
+
+}
+ */
