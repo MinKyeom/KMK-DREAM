758eea895cf9b7622908ad42d11d0db62ff1fbd8
MinKyeom <minkyeom@naver.com>
2025-09-08 08:23:24 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/배열 조작하기.java
@@ -0,0 +1,50 @@
+// package Programmers;
+
+// public class 배열 조작하기 {
+  
+// }
+
+// 출처:프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181893
+
+
+/*
+ * import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[] query) {
+        int[] answer = {};
+        
+        List<Integer> check = new ArrayList<>();
+        
+        for(int c: arr){
+            check.add(c);
+        }
+        
+        for(int a = 0 ; a< arr.length; a++){
+            if(a%2==0){
+                
+                for(int q: query){
+            
+                    for(int i = 0; i < check.size();){
+                
+            }
+        }
+            }
+            else{
+                
+                for(int q: query){
+
+                    for(int i = 0; i < check.size();){
+                
+            }
+        }
+            }
+        }
+        
+
+        
+        return answer;
+    }
+}
+ */
