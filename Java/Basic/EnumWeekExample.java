326f5f697e36cf8a3e9794f00287e05a114c5743
MinKyeom <minkyeom@naver.com>
2025-09-10 08:21:51 +0900

    Java Basic Study

---
+++ Java/Basic/EnumWeekExample.java
@@ -0,0 +1,16 @@
+package src.Java_Basic;
+
+import java.util.Calendar;
+import src.Java_Basic.Week;
+
+public class EnumWeekExample {
+
+  public static void main(String[] args){
+    Week today = null;
+
+    Calendar  cal = Calendar.getInstance();
+
+    System.out.println(Week.SUNDAY);
+  }
+
+}
---
+++ Java/Basic/Week.java
@@ -0,0 +1,12 @@
+package src.Java_Basic;
+
+public enum Week {
+    MONDAY,
+    TUESDAY,
+    WEDMESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY  
+}
+
