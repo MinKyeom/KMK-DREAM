8a61cef4401afa4c0e32c98f398ae263247a61d3
MinKyeom <minkyeom@naver.com>
2025-09-14 16:18:29 +0900

    Java Basic Study

---
+++ Programmer_Java/Lv0/n번째 원소부터.java
@@ -0,0 +1,22 @@
+// package Programmers;
+
+// public class n번째 원소부터 {
+  
+// }
+
+// 출처: 프로그래머스,
+// https://school.programmers.co.kr/learn/courses/30/lessons/181892
+
+// class Solution {
+//     public int[] solution(int[] num_list, int n) {
+//         int[] answer = new int[num_list.length-n+1];
+        
+//         for(int k = n-1; k < num_list.length; k++ ){
+//             answer[ k-(n-1) ] = num_list[k]; 
+//         }
+        
+//         return answer;
+//     }
+
+// }
+
