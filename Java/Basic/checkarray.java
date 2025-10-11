216e9c9e32d11fab17fd2d994f7799ffc9c2f081
MinKyeom <minkyeom@naver.com>
2025-10-11 20:43:56 +0900

    Java Basic Study

---
+++ Java/Basic/checkarray.java
@@ -0,0 +1,24 @@
+package src.Java_Basic;
+
+public class checkarray {
+
+
+  public static void check(int[] x, int[] y){
+    if(x==y){
+      System.out.print("O");
+    }
+
+    else{
+      System.out.print("N");
+    }
+
+  }
+
+  public static void main(String[] args){
+    int a[] = new int[] {1,2,3,4};
+    int b[] = new int[] {1,2,3,4};
+    int c[] = new int[] {1,2,3};
+
+  }
+  
+}
