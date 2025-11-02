b656ca5dc1d3b3b2d1fa5702cfa431f2e406c2ee
MinKyeom <minkyeom@naver.com>
2025-11-03 08:00:35 +0900

    Java Basic Study

---
+++ Java/Basic/2023_01_20.java
@@ -0,0 +1,33 @@
+package src.Java_Basic;
+
+class Parent {
+  int x = 1000;
+  Parent() {
+    this(3000);
+  }
+  Parent(int x){
+    this.x = x;
+  }
+}
+
+class Child extends Parent{
+  int x = 4000;
+  Child() {
+    this(5000);
+  }
+
+  Child(int x){
+    this.x =x;
+  }
+  int getX() {
+    return this.x
+  }
+}
+
+public class 2023_01_20 {
+  public static void main(String[] args){
+    Child c = new Child();
+    System.out.println(c.getX());
+  }
+  
+}
