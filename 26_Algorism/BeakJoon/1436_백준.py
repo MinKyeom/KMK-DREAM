e1e9529f61c476909ce71bcd5f3a5fe2bf09365a
minkyeom <minkyeom@naver.com>
2026-02-15 18:15:15 +0900

    Algorism_review

--- 26_Algorism/BeakJoon/1436_백준.py
+++ 26_Algorism/BeakJoon/1436_백준.py
@@ -21,3 +21,19 @@ answer = int(front + "666")
 
 print(answer)
 
+
+# 다른 사람 풀이
+# n = int(input())
+# cnt = 0
+# result = 666
+#  
+# while True:
+#     if '666' in str(result):
+#         cnt += 1
+#  
+#     if cnt == n:
+#         break
+#  
+#     result += 1
+#  
+# print(result)
