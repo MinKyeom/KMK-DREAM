5d00fdf43a469989aafeb93949060fcb845b427f
MinKyeom <minkyeom@naver.com>
2026-03-04 08:40:49 +0900

    Java Basic review

---
+++ JAVA STUDY/AdvancedForexample.java
@@ -0,0 +1,14 @@
+public class AdvancedForexample {
+  public static void main(String[] args){
+    int[] scores = {95,71};
+
+    int sum = 0;
+
+    for(int score: scores){
+      sum = sum + score;
+    }
+
+    System.out.println(sum);
+  }
+  
+}
---
+++ JAVA STUDY/StudentExample.java
@@ -0,0 +1,7 @@
+public class StudentExample {
+  public static void main(String[] args){
+    Student s1 = new Student();
+    System.out.println(s1);
+  }
+  
+}
