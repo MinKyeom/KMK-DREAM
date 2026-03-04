b928043ce204ef77f2444696899a0d908fd7a5d1
MinKyeom <minkyeom@naver.com>
2026-03-05 08:47:13 +0900

    Java Basic Review

---
+++ JAVA STUDY/CarExample.java
@@ -0,0 +1,7 @@
+public class CarExample {
+  public static void main(String[] args){
+    Car myCar = new Car();
+
+    System.out.println(myCar.company);
+  }
+}
---
+++ JAVA STUDY/KoreanExample.java
@@ -0,0 +1,7 @@
+public class KoreanExample {
+  public static void main(String[] args){
+    Korean k1 = new Korean("박","010");
+
+    System.out.println(k1.name + k1.ssn);
+  }
+}
