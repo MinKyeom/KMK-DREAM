295ac6b36bbdf6c64e9e76e8c3e0b4f09b59a946
MinKyeom <minkyeom@naver.com>
2026-03-11 08:59:34 +0900

    Java Basic review

--- JAVA STUDY/Car.java
+++ JAVA STUDY/Car.java
@@ -19,5 +19,21 @@ public class Car {
     this.color =color;
   }
 
-  
+
+// 메소드 
+int getSpeed(){
+  return speed;
+}
+
+void keyTurnOn(){
+  System.out.println("키를 돌립니다");
+}
+
+void run(){
+  for(int i = 0; i <= 50; i+=0){
+    speed = i;
+    System.out.println("달립니다."+speed);
+  }
+}
+
 }
--- JAVA STUDY/CarExample.java
+++ JAVA STUDY/CarExample.java
@@ -3,5 +3,11 @@ public class CarExample {
     Car myCar = new Car();
 
     System.out.println(myCar.company);
+
+    myCar.keyTurnOn();
+    myCar.run();
+
+    int speed =myCar.getSpeed();
+    System.out.println("현재 속도"+ speed);
   }
 }
