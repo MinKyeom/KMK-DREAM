1e9a009ec9a108a0815c1bcb968927d8325588b3
MinKyeom <minkyeom@naver.com>
2026-03-16 08:45:37 +0900

    Java Basic review

---
+++ JAVA STUDY/PersonExample.java
@@ -0,0 +1,9 @@
+public class PersonExample {
+  public static void main(String[] args){
+    Person p1 = new Person("123456-1234","check");
+
+    System.out.println(p1.name);
+
+    p1.name = "홍";
+  }
+}
