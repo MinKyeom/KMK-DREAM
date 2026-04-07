4fb2ae72ffe8c0b1f5a0dda1750cf2a060282b92
MinKyeom <minkyeom@naver.com>
2026-04-08 08:50:48 +0900

    Java Basic Review

---
+++ JAVA STUDY/AnimalExample.java
@@ -0,0 +1,21 @@
+public class AnimalExample {
+  public static void main(String[] args){
+    Dog dog = new Dog();
+
+    dog.sound();
+  
+    System.out.println("---");
+    
+    Animal animal = null;
+    animal = new Dog();
+    animal.sound();
+
+    System.out.println("---");
+
+    animalSound(new Dog());
+  }
+
+    public static void animalSound(Animal animal){
+      animal.sound();
+    }
+}
