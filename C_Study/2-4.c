d5090ab16b2d8d5d9f27c9452afbab548274db9f
MinKyeom <minkyeom@naver.com>
2026-04-10 21:13:07 +0900

    C study

---
+++ C_Study/2-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n",10);
+    printf("%lf\n",3.4);
+    printf("%1lf\n",3.45);
+    printf("%10f\n", 3.4);
+
+    printf("%d\n",12);
+    printf("%d\n",014);
+    printf("%d\n",0xc);
+
+    printf("%c",'A');
+    return 0;
+}
