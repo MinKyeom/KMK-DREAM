61323101da7f78d6d733154ae5d5c6c6cd944291
MinKyeom <minkyeom@naver.com>
2026-04-16 08:37:31 +0900

    Java Basic review

---
+++ JAVA STUDY/Test11.java
@@ -0,0 +1,39 @@
+class SuperObject{
+  public void draw(){
+    System.out.println('A');
+    draw();
+  }
+
+  public void paint(){
+    System.out.print('B');
+    draw();
+  }
+}
+
+
+class SubObject extends SuperObject{
+  public void paint(){
+    super.paint();
+    System.out.print('C');
+    draw();
+  }
+
+  public void draw(){
+    System.out.print('D');
+  }
+}
+
+
+
+
+
+public class Test11 {
+
+  public static void main(String[] args){
+    SuperObject a = new SubObject();
+
+    a.paint();
+    a.draw();
+  }
+  
+}
