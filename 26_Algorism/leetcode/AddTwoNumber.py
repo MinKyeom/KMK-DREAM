98542c08b8a1a655e121d85c67d9b6c9c897c996
MinKyeom <minkyeom@naver.com>
2026-04-20 08:46:03 +0900

    Algorism_review

---
+++ 26_Algorism/leetcode/AddTwoNumber.py
@@ -0,0 +1,29 @@
+# 출처:https://leetcode.com/problems/add-two-numbers/
+
+from collections import deque
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        node1 = l1
+        node2 = l2
+
+        num1 = ""
+        num2 = ""
+
+        while node1 != None :
+            num1 += str(node1.val)
+            node1 = node1.next
+
+        while node2 != None:
+            num2 += str(node2.val)
+            node2 = node2.next
+        
+        result = int(num1[::-1])+int(num2[::-1])
+        final = list(map(int,list(str(result)[::-1])))
+        
+        fianl_result = ListNode(final[0])
+        current = final_result
+
+        for i in range(1, len(final_result)):
+            final
