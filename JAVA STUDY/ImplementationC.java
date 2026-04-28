7054219dbff113b96e8fd6f88a5845d0501235c7
MinKyeom <minkyeom@naver.com>
2026-04-29 08:50:13 +0900

    Java Basic Review

---
+++ JAVA STUDY/ImplementationC.java
@@ -0,0 +1,15 @@
+public class ImplementationC implements InterfaceC {
+
+  public void methodA() {
+    System.out.println("InterfaceA");
+  }
+
+  public void methodB() {
+    System.out.println("InterfaceB");
+  }
+
+  public void methodC(){
+    System.out.println("InterfaceC");
+  }
+  
+}
---
+++ JAVA STUDY/InterfaceExample.java
@@ -0,0 +1,10 @@
+public class InterfaceExample {
+  public static void main(String[] args){
+    ImplementationC impl = new ImplementationC();
+
+    impl.methodA();
+    impl.methodB();
+    impl.methodC();
+  }
+}
+
