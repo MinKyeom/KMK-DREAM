45c58807456f77ecbb2e35d4321c92911a7e7586
MinKyeom <minkyeom@naver.com>
2026-05-07 08:45:21 +0900

    Java Basic Review

--- JAVA STUDY/Anonymous.java
+++ JAVA STUDY/Anonymous.java
@@ -13,7 +13,18 @@ public class Anonymous {
 
   };
 
-  
 
-  
+ // 익명 구현 객체 생성
+  RemoteControl field2 = new RemoteControl(){
+    @Override
+    public void turnOn(){
+      System.out.println("Tv On");
+    }
+
+    @Override
+    public void turnOff(){
+      System.out.println("Tv Off");
+    }
+  };
+
 }
--- JAVA STUDY/RemoteControl.java
+++ JAVA STUDY/RemoteControl.java
@@ -1,11 +1,17 @@
-public interface RemoteControl {
+// public interface RemoteControl {
   
-  public int MAX_VOLUME = 10;
-  public int MIN_VOLUME = 0;
+//   public int MAX_VOLUME = 10;
+//   public int MIN_VOLUME = 0;
 
-  //추상 메소드
+//   //추상 메소드
+//   public void turnOn();
+//   public void turnOff();
+//   public void setVolume(int volume);
+  
+// }
+
+public interface RemoteControl{
   public void turnOn();
   public void turnOff();
-  public void setVolume(int volume);
-  
 }
+
