0b2326a9fe825a9e5dc58fe73800449c5e7c2aa2
MinKyeom <minkyeom@naver.com>
2026-05-08 08:50:04 +0900

    Java Basic Review

--- JAVA STUDY/Anonymous.java
+++ JAVA STUDY/Anonymous.java
@@ -27,4 +27,8 @@ public class Anonymous {
     }
   };
 
-}
+  void method2(RemoteControl rc){
+    rc.turnOn();
+    }
+  }
+
--- JAVA STUDY/RemoteControl.java
+++ JAVA STUDY/RemoteControl.java
@@ -15,3 +15,4 @@ public interface RemoteControl{
   public void turnOff();
 }
 
+
