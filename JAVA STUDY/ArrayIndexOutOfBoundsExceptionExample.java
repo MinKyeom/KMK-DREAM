a8bc31b1908839339e8e2cfdafd40e7e06402253
MinKyeom <minkyeom@naver.com>
2026-05-13 08:46:42 +0900

    Java Basic Review

--- JAVA STUDY/ArrayIndexOutOfBoundsExceptionExample.java
+++ JAVA STUDY/ArrayIndexOutOfBoundsExceptionExample.java
@@ -1,7 +1,12 @@
 public class ArrayIndexOutOfBoundsExceptionExample {
   public static void main(String[] args){
+    if(args.length == 2){
     String data1 = args[0];
 
-    // System.out.println(data1);
+    System.out.println(data1);
+    }
+    else{
+      System.out.println("다시");
+    }
   }
 }
