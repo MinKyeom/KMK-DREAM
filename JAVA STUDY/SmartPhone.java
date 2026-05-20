7b382b9bee98e673785b88671d6700de349dd245
MinKyeom <minkyeom@naver.com>
2026-05-21 08:45:49 +0900

    Java Basic Review

--- JAVA STUDY/SmartPhone.java
+++ JAVA STUDY/SmartPhone.java
@@ -1,9 +1,26 @@
-public class SmartPhone extends Phone {
-  public SmartPhone(String owner){
-    super(owner);
-  }
+// public class SmartPhone extends Phone {
+//   public SmartPhone(String owner){
+//     super(owner);
+//   }
 
-  public void internetSearch(){
-    System.out.println("인터넷 검색을 합니다.");
-  }
+//   public void internetSearch(){
+//     System.out.println("인터넷 검색을 합니다.");
+//   }
+// }
+
+public class SmartPhone {
+    private String company;
+    private String os;
+
+    // 생성자
+    public SmartPhone(String company, String os){
+      this.company = company;
+      this.os = os;
+    }
+
+    @Override
+    public String toString(){
+      return company + "," + os;
+    }
+
 }
