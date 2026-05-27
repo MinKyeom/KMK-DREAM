a0789f8ce807ff96732a30e565f61d2500535c66
MinKyeom <minkyeom@naver.com>
2026-05-28 08:52:08 +0900

    Java Basic Review

---
+++ JAVA STUDY/ExitExample.java
@@ -0,0 +1,11 @@
+public class ExitExample {
+  public static void main(String[] args){
+    for(int i = 0; i < 0; i++){
+      if(i == 5){
+        System.exit(0);
+        //break;
+      }
+    }
+    System.out.println("마무리 코드");
+  }
+}
