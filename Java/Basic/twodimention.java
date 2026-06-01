96f3817e7a21cec1b5c90bcd2c85a0f132037a18
MinKyeom <minkyeom@naver.com>
2026-06-02 08:47:27 +0900

    Java Basic Review & Project Back up

---
+++ Java/Basic/twodimention.java
@@ -0,0 +1,12 @@
+package src.Java_Basic;
+
+public class twodimention {
+  public static void main(String[] args){
+  int[][] check = new int[2][3];
+
+  System.out.println(check[0].length);
+  System.out.println(check.length);
+
+  }
+  
+}
