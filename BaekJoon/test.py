75f710e666ef005862db5afaae5d386600759e86
minkyeom <minkyeom@naver.com>
2026-06-21 18:52:00 +0900

    Algorism

--- BaekJoon/test.py
+++ BaekJoon/test.py
@@ -1,39 +1,33 @@
- #
- # a = list(A)
- #    b = []
- #    count = 0
- #    z = 0
- #    while z == len(a):
- #        for x in range(len(a)):
- #            b.append(a[x - 1])
- #            print(b)
- #        z += 1
- #
- #        count += 1
- #        c = "".join(b)
- #        if c == B:
- #            answer = count
- #        break
- #
- #
- # def solution(A, B):
- #     a = list(A)
- #     b = []
- #     print(a)
- #     #     count=0
- #     #     z=0
- #     #         for x in range(len(a)):
- #     #             b.append(a[x-1])
- #     #         # count+=1
- #     # #         c="".join(b)
- #     # #         if c==B:
- #     # #             answer=count
- #     # #             break
- #
- #     # #         z+=1
- #
- #     # #     if not c==B:
- #     answer = -1
- #
- #
- # return answer
+from collections import deque
+c=int(input())
+check=int(input())
+g={}
+
+for t in range(1,c+1):
+    g[t]=[]
+
+
+for k in range(check):
+    i,j=list(map(int,input().split()))
+    if not j in g[i]:
+        g[i].append(j)
+    if not i in g[j]:
+        g[j].append(i)
+
+visit=[1]
+q=deque([1])
+
+def dfs():
+    v=q.popleft()
+
+    for w in g[v]:
+        if not w in visit:
+            visit.append(w)
+            q.append(w)
+            dfs()
+        else:
+            continue
+
+dfs()
+
+print(len(visit)-1)
